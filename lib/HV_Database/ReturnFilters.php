2ed83f453b5687bb5cd28f2f2e196bf959cabb44
Keith Hughitt <keith.hughitt@gmail.com>
2008-04-04 15:50:56 -0400

    progress save 04-04-2008: continued DataNavigator implementation.. (CURRENTLY DEBUGGING)

--- lib/HV_Database/ReturnFilters.php
+++ lib/HV_Database/ReturnFilters.php
@@ -2,13 +2,20 @@
 	include 'database_functions.php';
 
 	//$Observatory = $_POST["Observatory"];
-	$Instrument = $_POST["Instrument"];
 	//$Detector = $_POST["Detector"];
 	//$Measurement = $_POST["Measurement"];
-	$From = 	  $_POST["From"];
-	$To = 		  $_POST["To"];
-	$Increment =  $_POST["Increment"];
-	$Units =	  $_POST["Units"];
+	//$From = 	        $_POST["From"];
+	//$To = 		 	    $_POST["To"];
+	$Instrument = 	    $_POST["Instrument"];
+	$Year =				$_POST["Year"];
+	$Month =			$_POST["Month"];
+	$Day = 				$_POST["Day"];
+	$Hour =				$_POST["Hour"];
+	$Minute =			$_POST["Minute"];
+	$Second =			$_POST["Second"];
+	$IncrementDays =    $_POST["IncrementDays"];
+	$IncrementHours =   $_POST["IncrementHours"];
+	$IncrementMinutes = $_POST["IncrementMinutes"];
 
-	Filter($Observatory, $Instrument, $Detector, $Measurement, $From, $To, $Increment, $Units);
+	Filter($Observatory, $Instrument, $Detector, $Measurement, $Year, $Month, $Day, $Hour, $Minute, $Second, $IncrementDays, $IncrementHours, $IncrementMinutes);
 ?>
