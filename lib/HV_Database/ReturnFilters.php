0bb4b666ad9a1de7140c615ef47533702d6ecd4c
Keith Hughitt <keith.hughitt@gmail.com>
2008-04-09 16:54:49 -0400

    Nightly build 04-09-2008: IE7 Support, adjustments to Filter and DataNavigator.

--- lib/HV_Database/ReturnFilters.php
+++ lib/HV_Database/ReturnFilters.php
@@ -1,21 +1,76 @@
 <?php
 	include 'database_functions.php';
-
+	header("content-type:application/json");
+	
+	//BEFORE:  SELECT * FROM maps WHERE (Instrument = 'EIT' AND timestamp BETWEEN '2003-10-05 00:00:00' AND '2003-10-12 00:00:00') ORDER BY timestamp LIMIT 30";
+	//CURRENT: SELECT * FROM maps WHERE (Instrument = 'EIT' AND hour = '0' AND minute = '00') ORDER BY timestamp LIMIT 30
+	
 	//$Observatory = $_POST["Observatory"];
 	//$Detector = $_POST["Detector"];
 	//$Measurement = $_POST["Measurement"];
 	//$From = 	        $_POST["From"];
 	//$To = 		 	    $_POST["To"];
-	$Instrument = 	    $_POST["Instrument"];
-	$Year =				$_POST["Year"];
-	$Month =			$_POST["Month"];
-	$Day = 				$_POST["Day"];
-	$Hour =				$_POST["Hour"];
-	$Minute =			$_POST["Minute"];
-	$Second =			$_POST["Second"];
-	$IncrementDays =    $_POST["IncrementDays"];
-	$IncrementHours =   $_POST["IncrementHours"];
-	$IncrementMinutes = $_POST["IncrementMinutes"];
+	$Instrument = 	    $_GET["Instrument"];
+	$Year =				$_GET["Year"];
+	$Month =			$_GET["Month"];
+	$Day = 				$_GET["Day"];
+	$Hour =				$_GET["Hour"];
+	$Minute =			$_GET["Minute"];
+	$Second =			$_GET["Second"];
+	$IncrementDays =    $_GET["IncrementDays"];
+	$IncrementHours =   $_GET["IncrementHours"];
+	$IncrementMinutes = $_GET["IncrementMinutes"];
+	
 
-	Filter($Observatory, $Instrument, $Detector, $Measurement, $Year, $Month, $Day, $Hour, $Minute, $Second, $IncrementDays, $IncrementHours, $IncrementMinutes);
+	Filter ($Observatory, $Instrument, $Detector, $Measurement, $Year, $Month, $Day, $Hour, $Minute, $Second, $IncrementDays, $IncrementHours, $IncrementMinutes);
+	
+	function Filter ($observatory, $instrument, $detector, $measurement, $year, $month, $day, $hour, $minute, $second, $incrementDays, $incrementHours, $incrementMinutes)
+	{
+		$limit = 1;
+		
+		$query = "SELECT * FROM maps WHERE (";
+		
+		//Instrument
+		if ($instrument != null) {
+			$query .= "Instrument = '$instrument'";
+		}
+		
+		//Time Increment
+		if ($incrementHours == 1) {
+			$query .= "AND day = '$day' AND minute = '$minute'";
+			$limit = 24;
+		}
+		
+		if ($incrementDays == 1) {
+			$query .= "AND hour = '$hour' AND minute = '$minute'";
+			$limit = 30;
+		}
+		
+		if ($incrementYears == 1) {
+			$query .= "AND day = '1' AND hour = '$hour' AND minute = '$minute'";
+			$limit = 10;
+		}
+		
+		if ($incrementMinutes == 1) {
+			$query .= "AND month= '$month' AND day = '$day' AND hour = '$hour'";
+			$limit = 10;
+		}
+		
+		$query .= ") ORDER BY timestamp LIMIT $limit";
+		
+		$result = mysql_query($query);
+		
+		$resultArray = array();
+
+		while ($row = mysql_fetch_array($result, MYSQL_ASSOC))
+		{
+			array_push($resultArray, $row);
+		}
+		
+		//pad array to keep images in sync with observation time
+		//for (var $i = 0; $i < $resultArray.length; $i++) {...}
+
+		echo json_encode($resultArray);
+	}
+	
 ?>
--- lib/HV_Database/database_functions.php
+++ lib/HV_Database/database_functions.php
@@ -5,55 +5,7 @@
 		or die ('I cannot connect to the database because: ' . mysql_error());
 
 	mysql_select_db ("esahelio_svdb0");	
-	
-	//Instrument=EIT&From=2003-10-1&To=2003-10-7
-	//SELECT * FROM maps WHERE (Instrument = 'EIT' AND timestamp BETWEEN '2003-10-1 00:00:00' AND '2003-10-7 00:00:00') ORDER BY timestamp;
-	function Filter ($Observatory, $Instrument, $Detector, $Measurement, $Year, $Month, $Day, $Hour, $Minute, $Second, $IncrementDays, $IncrementHours, $IncrementMinutes)
-	{
-		$limit = 1;
 		
-		$Query = "SELECT * FROM maps WHERE (";
-		
-		//Instrument
-		if ($Instrument != null) {
-			$Query .= "Instrument = '$Instrument'";
-		}
-		
-		//Time Increment
-		if ($IncrementHours == 1) {
-			$Query .= "AND day = '$Day' AND minute = '$Minute'";
-			$limit = 24;
-		}
-		
-		if ($IncrementDays == 1) {
-			$Query .= "AND hour = '$Hour' AND minute = '$Minute'";
-			$limit = 30;
-		}
-		
-		if ($IncrementYears == 1) {
-			$Query .= "AND day = '1' AND hour = '$Hour' AND minute = '$Minute'";
-			$limit = 10;
-		}
-		
-		if ($IncrementMinutes == 1) {
-			$Query .= "AND month= '$Month' AND day = '$Day' AND hour = '$Hour'";
-			$limit = 10;
-		}
-		
-		//$Query .= "AND timestamp BETWEEN '$From 00:00:00' AND '$To 00:00:00') ORDER BY timestamp LIMIT $limit";
-		$Query .= ") ORDER BY timestamp LIMIT $limit";
-		
-		$Result = mysql_query($Query);
-		$ResultStore = array();
-
-		while ($Row = mysql_fetch_array($Result, MYSQL_NUM))
-		{
-			array_push($ResultStore, $Row);
-		}
-
-		echo json_encode($ResultStore);
-	}
-	
 	function ReturnInstruments()
 	{
 		$Query = "SELECT DISTINCT instrument FROM maps ORDER BY instrument";
