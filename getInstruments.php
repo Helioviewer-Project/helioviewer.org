f3596bd11a6a9d8d76b4a51af28184c85fb5e073
V. Keith Hughitt <hughitt1@Kore>
2008-06-18 15:04:01 -0400

    nightly build 06-18-2008: New calendar. Server-side HTML Generation for Layer Manager Instrument Field.

--- getInstruments.php
+++ getInstruments.php
@@ -1,9 +1,9 @@
 <?php
 
-header("Content-type: application/json");
 require('phpClasses/autoload.php');
-
 $dbConnection = new DbConnection();
+$return = $_GET['type'];
+
 $query = "SELECT DISTINCT instrument.name as instrument, " .
          "detector.name as detector, " .
          "observatory.name as observatory, " .
@@ -20,5 +20,25 @@ $instruments = array();
 while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
     array_push($instruments, $row);
 }
-echo json_encode($instruments);
+
+if ($return == "json") {
+    header("Content-type: application/json");
+    echo json_encode($instruments);
+}
+
+else if ($return == "html") {
+?>
+<div style="float: left; width: 40%; color: white;">Instrument:</div>
+<div style="float: right; width: 60%;">
+    <select class='instrument-select'>
+        <option value="explanation">Select Instrument</option>
+<?php
+    foreach ($instruments as $inst) {
+        echo "        <option value='$inst[instrument]'>$inst[instrument]</option>\n";
+    }
+?>
+    </select>
+</div>
+<?php
+}
 ?>
