fc3cc4a995cc31f17fa6696a10a956a5a3cdaa53
V. Keith Hughitt <hughitt1@Kore>
2008-07-18 12:19:01 -0400

    nightly build 07-18-2008: Fixed bugs #248495 (Zooming inconsistent when image uncentered) and #248435 (visible gaps between image loads when navigating by time).

--- getEvents.php
+++ getEvents.php
@@ -1,6 +1,6 @@
 <?php
     header("Content-type: application/json");
     //$url = "http://localhost:8080/Dispatcher/resources/eventCatalogs?" . $_SERVER['QUERY_STRING'];
-    $url = "http://141.3.69.108:8080/Dispatcher/resources/eventCatalogs?" . $_SERVER['QUERY_STRING'];
+    $url = "http://washington.tm.uni-karlsruhe.de/Dispatcher/resources/eventCatalogs?" . $_SERVER['QUERY_STRING'];
     echo file_get_contents($url);
 ?> 
