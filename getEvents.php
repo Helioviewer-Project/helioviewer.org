eab615aa32b84a28f46d1615d88a924cdbe7e50a
V. Keith Hughitt <hughitt1@Kore>
2008-07-25 16:11:38 -0400

    nightly build 07-25-2008: Bug fixes (#248496 and #251920).

--- getEvents.php
+++ getEvents.php
@@ -3,4 +3,4 @@
     //$url = "http://localhost:8080/Dispatcher/resources/eventCatalogs?" . $_SERVER['QUERY_STRING'];
     $url = "http://washington.tm.uni-karlsruhe.de/Dispatcher/resources/eventCatalogs?" . $_SERVER['QUERY_STRING'];
     echo file_get_contents($url);
-?> 
+?>
