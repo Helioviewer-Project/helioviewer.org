e0cde56db4dec18bebb0e0e432372bd6b79334f7
V. Keith Hughitt <hughitt1@Kore>
2008-10-03 16:14:27 -0400

    nightly build 10-03-2008

--- getLayerAvailability.php
+++ getLayerAvailability.php
@@ -1,23 +1,75 @@
 <?php
-require('phpClasses/autoload.php');
-$dbConnection = new DbConnection();
+	require('phpClasses/autoload.php');
+	$dbConnection = new DbConnection();
+	
+	// Layer parameters
+	$obs =  $_GET['observatory'];
+	$inst = $_GET['instrument'];
+	$det  = $_GET['detector'];
+	$meas = $_GET['measurement'];
+	
+	// Return format
+	$return = $_GET['format'];
+	
+	// Query
+	$query = "SELECT DISTINCT instrument.name as instrument, detector.name as detector, observatory.name as observatory, measurement.name as measurement 
+	FROM observatory
+	INNER JOIN instrument ON observatory.id = instrument.observatoryId
+	INNER JOIN detector ON detector.instrumentId = instrument.id
+	INNER JOIN measurement ON measurement.detectorId = detector.id";
 
-//return format
-$return = $_GET['type'];
+	if (($obs != "none") OR ($inst != "none") OR ($det != "none") OR ($meas != "none"))
+		$query .= " WHERE";
+	
+	// Limit results
+	if ($obs != "none")
+		$query .= " observatory.name = '$obs'";
+	
+	if ($inst != "none") {
+		if (substr($query, -5) != "WHERE")
+			$query .= " AND";
+		$query .= " instrument.name = '$inst'";
+	}
+	
+	if ($det != "none") {
+		if (substr($query, -5) != "WHERE")
+			$query .= " AND";
+		$query .= " detector.name = '$det'";
+	}
+	
+	if ($meas != "none") {
+		if (substr($query, -5) != "WHERE")
+			$query .= " AND";
+		$query .= " measurement.name = '$meas'";
+	}
+	$query .= ";";
+		
+	// Store results
+	$observatories = array();
+	$instruments   = array();
+	$detectors     = array();
+	$measurements  = array();
 
-//master array
-$observatories = array();
-
-//query observatories
-$query = "SELECT name FROM observatory";
-$result = $dbConnection->query($query);
-while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-    array_push($observatories, $row["name"]);
-}
-
-if ($return == "json") {
-    //header("Content-type: application/json");
-    echo json_encode($observatories);
-}
-
+	// Perform query
+	$result = $dbConnection->query($query);
+	while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+		array_push($observatories, $row['observatory']);
+		array_push($instruments, $row['instrument']);
+		array_push($detectors, $row['detector']);
+		array_push($measurements, $row['measurement']);
+	}
+	
+	// Remove redundent entries
+	$settings = array();
+	$settings['observatories'] = array_keys(array_flip($observatories));
+	$settings['instruments'] =   array_keys(array_flip($instruments));
+	$settings['detectors'] =     array_keys(array_flip($detectors));
+	$settings['measurements'] =  array_keys(array_flip($measurements));
+	
+	// JSON
+	if ($return == "json") {
+		header("Content-type: application/json");
+		echo json_encode($settings);
+	}
 ?>
+
