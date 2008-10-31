0914464824729aa75a839d468a83fa0a935f0e4a
V. Keith Hughitt <hughitt1@Kore>
2008-10-31 15:46:40 -0400

    nightly build 10-31-2008

--- api/getEvents.php
+++ api/getEvents.php
@@ -1,5 +1,5 @@
 <?php
     header("Content-type: application/json");
-    $url = "http://washington.tm.uni-karlsruhe.de/Dispatcher/resources/eventCatalogs?" . $_SERVER['QUERY_STRING'];
+    $url = "http://washington.tm.uni-karlsruhe.de:8080/Dispatcher/resources/eventCatalogs?" . $_SERVER['QUERY_STRING'];
     echo file_get_contents($url);
 ?>
