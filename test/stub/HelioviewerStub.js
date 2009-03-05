28d8f50176d6bb204851edfa1f63cc10645b3bf7
V. Keith Hughitt <hughitt1@Kore>
2009-03-05 09:10:12 -0500

    nightly build 03-05-2009

---
+++ test/stub/HelioviewerStub.js
@@ -0,0 +1,15 @@
+/**
+ * @description HelioviewerStub class definition
+ */
+HelioviewerStub = function () {
+	this.defaultZoomLevel = 12;
+	this.baseZoom  = 10;
+	this.baseScale = 2.63;
+	this.defaultPrefetchSize = 0;
+	this.timeIncrementSecs = 86400;
+	this.minZoomLevel = 8;
+	this.maxZoomLevel = 16;
+	this.tileAPI = 'api/index.php';
+	this.imageAPI = 'api/index.php';
+	this.eventAPI = 'api/index.php';	
+};
