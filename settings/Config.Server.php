f3dc8001824d555d459c7e9c71035ccceb04819f
V. Keith Hughitt <hughitt1@Kore>
2009-04-22 16:00:44 -0400

    nightly build 04-22-2009

--- settings/Config.Server.php
+++ settings/Config.Server.php
@@ -7,8 +7,8 @@
 class Config {
     
   	// Version Information
-	const LAST_UPDATE		 = '2009/04/21';
-	const BUILD_NUM          = 221;
+	const LAST_UPDATE		 = '2009/04/22';
+	const BUILD_NUM          = 222;
     
    	// Viewer
 	const DEFAULT_ZOOM_LEVEL = 11;
