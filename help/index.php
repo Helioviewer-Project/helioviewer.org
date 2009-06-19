3f1174acb859d192c239503756af636bddc68188
V. Keith Hughitt <hughitt1@Kore>
2009-06-19 13:54:23 -0400

    pre-merge commit: 2009/06/19

--- help/index.php
+++ help/index.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<title>Helioviewer > Help</title>
-		<link rel="shortcut icon" href="favicon.ico">
+		<link rel="shortcut icon" href="../favicon.ico">
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<meta name="description" content="Helioviewer Help">
 		<meta name="keywords" content="Helioviewer, help, hv, jpeg 2000, jp2, solar image viewer, sun, solar, heliosphere, solar physics, viewer, visualization, space, astronomy, SOHO, EIT, LASCO, SDO, MDI, coronagraph, ">
@@ -24,9 +24,75 @@
 		
 		<!-- Theme Modifications -->
 		<link rel="stylesheet" type="text/css" href="../styles/dot-luv.css">
+        
+        <style type="text/css">
+            body {
+            }
+            
+            #help-outer-container {
+                margin-left: auto;
+                margin-right: auto;
+                text-align: center;
+                width: 80%;
+                height: 98%;
+                border: 1px solid red;
+                position: relative;
+            }
+            
+            #help-title {
+                height: 80px;
+                text-align: left;
+                margin-top: 20px;
+            }
+            #help-nav-menu {
+                background-image: url(../images/blackGlass/transparentBG50.png);
+                border: 1px solid blue;
+                width:20%;
+                height: 85%;
+                margin-right: 10%;
+                float: left;
+            }
+            #help-contents {
+                background-image: url(../images/blackGlass/transparentBG50.png);
+                border: 1px solid green;
+                height: 85%;
+                width:60%;
+                position: absolute;
+                left: 21%;
+                width: 79%;
+            }
+            
+            #help-nav-menu-list {
+                color: white;
+                text-align: left;
+                line-height: 200%;
+                font-size: 1.2em;
+            }      
+        </style>
     </head>
     
     <body>
-        
+        <div id="help-outer-container">
+            <!-- Title -->
+            <div id="help-title">
+                <img style="float: left;" src="../images/logos/about.png" alt="Helioviewer.org Logo"/>
+                <h1 style="position: relative; top: 22px; font-size: 2.8em; color: #5D6061; font-family: arial, verdana, sans-serif; ">Help</h1>
+            </div>
+            
+            <!-- Menu -->
+            <div id="help-nav-menu">
+                <ul id="help-nav-menu-list">
+                    <li>Getting Started</li>
+                    <li>Basic Usage</li>
+                    <li>Working with images</li>
+                    <li>Working with Features & Events</li>
+                    <li>Navigating by time</li>
+                    <li>Movies</li>
+                </ul>
+            </div>
+            
+            <!-- Contents -->
+            <div id="help-contents"></div>        
+        </div>
     </body>
 </html>      
