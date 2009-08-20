2e278fe736e31e9fe70e7b3a29ec50f02f665baf
V. Keith Hughitt <hughitt1@Kore>
2009-08-20 11:55:05 -0400

    progress save 2009/08/20: Switched to QWizard in install.py

--- install/install.py
+++ install/install.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import sys
 from PyQt4 import QtCore, QtGui
-from org.helioviewer.InstallDialog import Ui_InstallDialog
+from org.helioviewer.InstallWizard import Ui_InstallWizard
 from org.helioviewer.jp2 import *
 from org.helioviewer.db  import *
 from org.helioviewer.utils import *
@@ -10,22 +10,23 @@ from org.helioviewer.utils import *
 #
 # Main Application Window
 #
-class HelioviewerInstallDialog(QtGui.QDialog):
+class HelioviewerInstallWizard(QtGui.QWizard):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
-        self.ui = Ui_InstallDialog()
+        self.ui = Ui_InstallWizard()
         self.ui.setupUi(self)
         self.initEvents()
         
     def initEvents(self):
-    	QtCore.QObject.connect(self.ui.jp2BrowseBtn, QtCore.SIGNAL("clicked()"), self.openBrowseDialog)
-    	QtCore.QObject.connect(self.ui.startBtn, QtCore.SIGNAL("clicked()"), self.validateFields)
-    	
-    	#QtCore.QObject.connect(self.ui.jp2RootDirInput, QtCore.SIGNAL("textChanged()"), self.checkJP2Dir) Not working?
-    	#QtCore.QObject.connect(self.ui.jp2RootDirInput, QtCore.SIGNAL("textEdited()"), self.openBrowseDialog)
-							   
+        print "Initializing..."
+        #self.ui.wizardPage1.setPixmap(QtGui.QWizard.LogoPixmap, QtGui.QPixmap(":/Logos/color.png"))
+        #QtCore.QObject.connect(self.ui.jp2BrowseBtn, QtCore.SIGNAL("clicked()"), self.openBrowseDialog)
+        #QtCore.QObject.connect(self.ui.startBtn, QtCore.SIGNAL("clicked()"), self.validateFields)
+        #QtCore.QObject.connect(self.ui.jp2RootDirInput, QtCore.SIGNAL("textChanged()"), self.checkJP2Dir) Not working?
+        #QtCore.QObject.connect(self.ui.jp2RootDirInput, QtCore.SIGNAL("textEdited()"), self.openBrowseDialog)
+
     def openBrowseDialog(self):
-    	fd = QtGui.QFileDialog(self)
+        fd = QtGui.QFileDialog(self)
     	directory = fd.getExistingDirectory()
     	self.ui.jp2RootDirInput.setText(directory)
 
@@ -37,7 +38,7 @@ class HelioviewerInstallDialog(QtGui.QDialog):
 
 def main(args):
     app = QtGui.QApplication(sys.argv)
-    win = HelioviewerInstallDialog()
+    win = HelioviewerInstallWizard()
     win.show()
     sys.exit(app.exec_())
 
@@ -92,3 +93,12 @@ def getDBInfo():
 
 if __name__ == '__main__':
 	main(sys.argv)
+
+#
+# Notes:
+#
+# 1. Using Resource files, see http://www.opensubscriber.com/message/pyqt@riverbankcomputing.com/8178240.html
+# 2. Post-compile modifications:
+#    - Add InstallWizard.setPixmap(QtGui.QWizard.LogoPixmap, QtGui.QPixmap(":/Logos/color.png")) to InstallWizard.py
+#    - change "import hv_rc" to "import resources.hv_rc"#
+#
