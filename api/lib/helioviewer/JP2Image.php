4757b11fae54c1bff0dfbcf99d48204e7bb88f3f
Keith Hughitt <hughitt1@kore>
2009-09-16 16:58:52 -0400

    nightly build 2009/09/16

--- api/lib/helioviewer/JP2Image.php
+++ api/lib/helioviewer/JP2Image.php
@@ -29,8 +29,6 @@ abstract class JP2Image {
     protected $baseScale    = 2.63; //Scale of an EIT image at the base zoom-level: 2.63 arcseconds/px
     protected $baseZoom     = 10;   //Zoom-level at which (EIT) images are of this scale.
     
-    protected $xRange;
-    protected $yRange;
     protected $zoomLevel;
     protected $tileSize;
 	protected $format;
@@ -43,25 +41,15 @@ abstract class JP2Image {
     protected $jp2Height;
     protected $jp2Scale;
     
-    protected $image;
-	protected $imageWidth;
-	protected $imageHeight;
-	protected $imageRelWidth;
-	protected $imageRelHeight;
-	
 	private $colorTable;
         
     /**
      * @param object file -- Location of the JPEG 2000 image to work with
      * @param int zoomLevel -- The zoomlevel to work with
-     * @param array xRange -- an associative array containing "xStart" (starting coordinate) and "xSize" (width)
-     * @param array yRange -- An associative array containing "yStart" (staring coordinate) and "ySize" (height)
      * @param array imageSize -- an associative array containing "width" and "height" of the image
      * @param boolean isTile -- whether the image is a tile or not
      */
-    protected function __construct($file, $xRange, $yRange, $zoomLevel, $imageSize, $width, $height, $scale, $format, $isTile = true) {
-        $this->xRange    = $xRange;
-        $this->yRange    = $yRange;
+    protected function __construct($file, $zoomLevel, $imageSize, $width, $height, $scale, $format) {
         $this->zoomLevel = $zoomLevel;
 		$this->jp2Width  = $width;
 		$this->jp2Height = $height;
@@ -268,110 +256,6 @@ abstract class JP2Image {
         }
         
         return $pgm;
-    }
-
-    /**
-     * getRegionString
-     * Build a region string to be used by kdu_expand. e.g. "-region {0.0,0.0},{0.5,0.5}"
-     * 
-     * NOTE: Because kakadu's internal precision for region strings is less than PHP,
-     * the numbers used are cut off to prevent erronious rounding.
-     * 
-     * 6-12-2009 Converted from using tile coordinates to pixels.
-     */
-	private function getRegionString() {
-		$precision = 6;
-
-		// Calculate the top, left, width, and height in terms of kdu_expand parameters (between 0 and 1)
-		$top 	= substr($this->yRange["start"] / $this->jp2Height, 0, $precision);	
-		$left 	= substr($this->xRange["start"] / $this->jp2Width,  0, $precision);
-		$height = substr($this->yRange["size"]   / $this->jp2Height, 0, $precision);
-		$width 	= substr($this->xRange["size"]   / $this->jp2Width,  0, $precision);
-		
-        $region = "-region \{$top,$left\},\{$height,$width\}";
-
-        return $region;		
-	}    
-
-	/** 
-	 * 6-12-2009 converted from using tile coordinates to pixels.   
-	 * If the image is a Tile, it is padded according to where it lies in the image.
-	 * If the image is a SubFieldImage, the image is padded with an offset from the NW corner.
-	 */ 
-    private function padImage ($jp2Width, $jp2Height, $width, $height, $x, $y) {		
-		if($this->isTile) {
-	        // Determine min and max tile numbers
-	        $imgNumTilesX = max(2, ceil($jp2Width  / $this->imageWidth));
-	        $imgNumTilesY = max(2, ceil($jp2Height / $this->imageHeight));
-	        
-	        // Tile placement architecture expects an even number of tiles along each dimension
-	        if ($imgNumTilesX % 2 != 0)
-	            $imgNumTilesX += 1;
-	
-	        if ($imgNumTilesY % 2 != 0)
-	            $imgNumTilesY += 1;
-
-			$numInnerTilesX = $imgNumTilesX - 2;
-			$numInnerTilesY = $imgNumTilesY - 2;
-
-	 		$tileMinX = ($this->jp2Width  / 2) - ($width  * $numInnerTilesX / 2);
-			$tileMaxX = ($this->jp2Width  / 2) + ($width  * $numInnerTilesX / 2);     
-			$tileMinY = ($this->jp2Height / 2) - ($height * $numInnerTilesY / 2); 
-			$tileMaxY = ($this->jp2Height / 2) + ($height * $numInnerTilesY / 2);   
-
-	        // Determine where the tile is located (where tile should lie in the padding)
-	        $gravity = null;
-
-	        if ($x < $tileMinX) {
-	            if ($y < $tileMinY) {
-	                $gravity = "SouthEast";
-	            }
-	            else if ($y == $tileMaxY) {
-	                $gravity = "NorthEast";
-	            }
-	            else {
-	                $gravity = "East";
-	            }
-	        }
-			
-	        else if ($x == $tileMaxX) {
-	            if ($y < $tileMinY) {
-	                $gravity = "SouthWest";
-	            }
-	            else if ($y == $tileMaxY) {
-	                $gravity = "NorthWest"; 
-	            }
-	            else {
-	                $gravity = "West";
-	            }
-	        }
-	        
-	        else {
-	            if($y < $tileMinY) {
-	            	$gravity = "South";
-	            }
-
-	            else {
-	                $gravity = "North";
-	            }
-	        }
-
-			$offset = " ";
-		}
-	
-		/* 
-		 * If the item is a subfieldImage, it is assumed that the overall picture is larger than, but contains this image.
-		 * The image has a heliocentric offset and will be padded with that offset. 
-		 */
-		else {
-			$gravity = "NorthWest";
-			// Offset the image from the center using the heliocentric offset
-			$offset  = $this->hcOffset["x"] . $this->hcOffset["y"] . " ";
-		}
-
-        // Construct padding command
-        // TEST: use black instead of transparent for background?
-        return "-gravity $gravity -extent " . $width . "x" . $height . $offset;
     }
     
 	/**
