01c555388e4ea393bfd84fc5599f7df1bdc2a55b
Keith Hughitt <keith.hughitt@nasa.gov>
2010-02-02 16:38:13 -0500

    Modifying back-end code to conform to PEAR Coding standards. Added build target to install/build.xml to generate PHP documentation.

--- api/Config.php
+++ api/Config.php
@@ -1,74 +1,110 @@
 <?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+/**
+ * Helioviewer Configuration Helper
+ * 
+ * This file defines a class which helps to parse Helioviewer's configuration 
+ * file, create global constants which can be used to access those configuration
+ * parameters, and fix data types for known parameters.
+ * 
+ * PHP version 5
+ * 
+ * @category Configuration
+ * @package  Helioviewer
+ * @author   Keith Hughitt <keith.hughitt@nasa.gov>
+ * @license  http://www.mozilla.org/MPL/MPL-1.1.html Mozilla Public License 1.1
+ * @link     http://launchpad.net/helioviewer.org
+ */
 class Config
 {
-    private $bools  = array("distributed_tiling_enabled", "backup_enabled", "disable_cache");
-    private $ints   = array("build_num", "default_zoom_level", "default_timestep", "min_zoom_level", "max_zoom_level", "prefetch_size",
-                            "png_compression_quality", "jpeg_compression_quality", "bit_depth", "num_colors", "tile_pad_width", 
-                            "max_movie_frames", "max_jpx_frames", "base_zoom_level");
-    private $floats = array("base_image_scale");
+    private $_bools  = array("distributed_tiling_enabled", "backup_enabled", 
+                             "disable_cache");
+    private $_ints   = array("build_num", "default_zoom_level", "bit_depth",
+                             "default_timestep", "min_zoom_level", 
+                             "max_zoom_level", "prefetch_size", "num_colors",
+                             "png_compression_quality", "tile_pad_width",
+                             "jpeg_compression_quality", "max_jpx_frames", 
+                             "max_movie_frames", "base_zoom_level");
+    private $_floats = array("base_image_scale");
     
     public  $servers;
     
-    public function __construct($file) {
+    /**
+     * Creates an instance of the Config helper class 
+     * 
+     * @param string $file Path to configuration file
+     */
+    public function __construct($file)
+    {
         $this->config = parse_ini_file($file);
         
-        $this->fixTypes();
+        $this->_fixTypes();
 
-        foreach($this->config as $key => $value) {
-        	if ($key !== "tile_server")
+        foreach ($this->config as $key => $value) {
+            if ($key !== "tile_server") {
                 define("HV_" . strtoupper($key), $value);
+            }
         }
         
-        foreach($this->config["tile_server"] as $id => $url) {
-        	define("HV_TILE_SERVER_$id", $url);
+        foreach ($this->config["tile_server"] as $id => $url) {
+            define("HV_TILE_SERVER_$id", $url);
         }
             
-        $this->setAdditionalParams();
+        $this->_setAdditionalParams();
             
-        $this->setupLogging(true);
+        $this->_setupLogging(true);
             
         $dbconfig = substr($file, 0, strripos($file, "/")) . "/Database.php";
-        require_once($dbconfig);
         
-        
+        include_once $dbconfig;        
     }
     
     /**
      * Casts known configuration variables to correct types.
-     * @return 
      */
-    private function fixTypes() {
+    private function _fixTypes()
+    {
         // booleans
-        foreach($this->bools as $boolean)
+        foreach ($this->_bools as $boolean) {
             $this->config[$boolean] = (bool) $this->config[$boolean];
+        }
             
         // integers
-        foreach($this->ints as $int)
+        foreach ($this->_ints as $int) {
             $this->config[$int] = (int) $this->config[$int];
+        }
         
         // floats
-        foreach($this->floats as $float)
+        foreach ($this->_floats as $float) {
             $this->config[$float] = (float) $this->config[$float];
+        }
     }
     
     /**
      * Makes sure that error log exists and selects desired logging verbosity
+     * 
+     * @param bool $verbose Whether or not to force verbose logging.
      */
-    private function setupLogging($verbose) {
-    	if ($verbose)
-    	   error_reporting(E_ALL | E_STRICT);
-	    $errorLog = HV_ERROR_LOG;
-	    if(!file_exists($errorLog))
-	        touch($errorLog);
+    private function _setupLogging($verbose)
+    {
+        if ($verbose) {
+            error_reporting(E_ALL | E_STRICT);
+        }
+        $errorLog = HV_ERROR_LOG;
+        if (!file_exists($errorLog)) {
+            touch($errorLog);
+        }
     }
     
     /**
      * Some useful values can be determined automatically...
-     * @return 
+     * 
      */
-    private function setAdditionalParams() {
+    private function _setAdditionalParams()
+    {
         //define("HV_ROOT_DIR", substr(getcwd(), 0, -4));
-        //define("HV_WEB_ROOT_URL", "http://" . $_SERVER["SERVER_NAME"] . substr($_SERVER["SCRIPT_NAME"], 0, -14));
+        //define("HV_WEB_ROOT_URL", "http://" . $_SERVER["SERVER_NAME"] 
+        //    . substr($_SERVER["SCRIPT_NAME"], 0, -14));
         define("HV_TMP_ROOT_DIR", HV_ROOT_DIR . "/tmp");
         define("HV_CACHE_DIR",    HV_ROOT_DIR . "/cache");
         define("HV_ERROR_LOG",    HV_ROOT_DIR . "/log/error");
@@ -76,4 +112,3 @@ class Config
         define("HV_TMP_ROOT_URL", HV_WEB_ROOT_URL . "/tmp");
     }
 }
-?>
--- api/modules/interface.Module.php
+++ api/modules/interface.Module.php
@@ -1,10 +1,30 @@
 <?php 
-
-interface Module 
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+/**
+ * Helioviewer Module Interface
+ * 
+ * PHP version 5
+ * 
+ * @category Configuration
+ * @package  Helioviewer
+ * @author   Keith Hughitt <keith.hughitt@nasa.gov>
+ * @author   Jonathan Harper <jwh376@msstate.edu>
+ * @license  http://www.mozilla.org/MPL/MPL-1.1.html Mozilla Public License 1.1
+ * @link     http://launchpad.net/helioviewer.org
+ */
+interface Module
 {
-	public function execute();
-	public function validate();
-	public static function printDoc();
+    /**
+     * Executes the requested action
+     */
+    public function execute();
+    /**
+     * Contains logic neccessary to validate any action supported by a given
+     * module
+     */
+    public function validate();
+    /**
+     * Prints documentation describe all of the actions provided by the module
+     */
+    public static function printDoc();
 }
-
-?>
