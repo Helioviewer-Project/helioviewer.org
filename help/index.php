51ee841f814de83298e1000cfb71a4f282b32811
Keith Hughitt <keith.hughitt@nasa.gov>
2010-03-25 13:43:22 -0400

    Updated references to old jQuery UI in several places.

--- help/index.php
+++ help/index.php
@@ -12,10 +12,10 @@
 
 		<!-- jQuery -->
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js" type="text/javascript"></script>
-		<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.1/jquery-ui.min.js" type="text/javascript"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js" type="text/javascript"></script>
 		<script src="../lib/jquery/jquery-tooltip/jquery.tooltip.js" type="text/javascript"></script>
 
-		<link rel="stylesheet" href="../lib/jquery/jquery.ui-1.7.1/css/dot-luv-modified/jquery-ui-1.7.1.custom.css" type="text/css" />	
+		<link rel="stylesheet" href="../lib/jquery/jquery.ui-1.8/css/dot-luv-modified/jquery-ui-1.8.custom.css" type="text/css" />	
         
   		<!-- Helioviewer.org custom styles -->
 		<link rel="stylesheet" type="text/css" href="../styles/main.css">
