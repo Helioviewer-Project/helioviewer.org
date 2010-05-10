f338b9f3c661173d5cfd2edecc5a122235e2abf5
Keith Hughitt <hughitt1@kore>
2010-05-10 10:24:33 -0400

    2010/05/10: TileLayer refactoring progress save.

--- src/Helioviewer.js
+++ src/Helioviewer.js
@@ -140,7 +140,7 @@ var Helioviewer = Class.extend(
             $.each(self.userSettings.get('tileLayers'), function (index, layer) {
                 $.extend(layer, dataSources[layer.observatory][layer.instrument][layer.detector][layer.measurement]);
                 self.tileLayers.addLayer(
-                    new TileLayer(self, self.getDate(), self.viewport.tileSize, self.api, 
+                    new TileLayer(self, index, self.getDate(), self.viewport.tileSize, self.api, 
                                   self.tileServers[layer.server], layer.observatory, layer.instrument, layer.detector,  
                                   layer.measurement, layer.sourceId, layer.name, layer.visible, layer.opacity,
                                   layer.layeringOrder, layer.server)
