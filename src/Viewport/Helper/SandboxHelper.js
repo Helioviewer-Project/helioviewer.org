08166d84f8158959150d456e1253d0ade43467b4
Jaclyn Beck <jaclyn.r.beck@gmail.com>
2010-07-15 11:51:46 -0400

    Redundant movie frames are now removed before creation to optimize movie generation. Made a bug fix to image positioning after zooming in/out.

--- src/Viewport/Helper/SandboxHelper.js
+++ src/Viewport/Helper/SandboxHelper.js
@@ -63,15 +63,15 @@ var SandboxHelper = Class.extend(
         // Update moving container position
         newHCLeft = Math.max(0, Math.min(desiredSandboxSize.width,  containerPos.left + (0.5 * change.x)));
         newHCTop  = Math.max(0, Math.min(desiredSandboxSize.height, containerPos.top  + (0.5 * change.y)));
-   
+ 
         this.moveContainerTo(newHCLeft, newHCTop);
     },
         
     moveContainerTo: function (x, y) {
-        if (x > 0) {
+        if (x > 0 || this.movingContainer.css('left').slice(0,-2) !== "0") {
             this.movingContainer.css({left: x + 'px'});
         }
-        if (y > 0) {
+        if (y > 0 || this.movingContainer.css('top').slice(0,-2) !== "0") {
             this.movingContainer.css({top:  y + 'px'});
         }
     }
