ce5a3fa72231bc7dfa5b14b79a2f062a4ae2b1e5
Keith Hughitt <keith.hughitt@nasa.gov>
2010-11-04 12:30:26 -0400

    Moved JavaScript to bottom of page.

--- index.php
+++ index.php
@@ -12,70 +12,28 @@ if ((!file_exists($ini)) || (!$config = parse_ini_file($ini)))
     <meta http-equiv="X-UA-Compatible" content="ie=9" />
     <meta http-equiv="X-UA-Compatible" content="ie=8" />
     <meta http-equiv="X-UA-Compatible" content="chrome=1" />
+    <meta name="description" content="Helioviewer - Solar and heliospheric image visualization tool" />
+    <meta name="keywords" content="Helioviewer, jpeg 2000, jp2, solar image viewer, sun, solar, heliosphere, solar physics, viewer, visualization, space, astronomy, SOHO, EIT, LASCO, SDO, MDI, coronagraph, " />
+    
+    <?php if ($config["disable_cache"]) echo "<meta http-equiv=\"Cache-Control\" content=\"No-Cache\" />\n"; ?>
+    
+    <link rel="shortcut icon" href="favicon.ico" />
+        
     <!--[if IE]>
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/chrome-frame/1/CFInstall.min.js"></script>
     <script type="text/javascript" src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
 
-    <link rel="shortcut icon" href="favicon.ico" />
-
-    <meta name="description" content="Helioviewer - Solar and heliospheric image visualization tool" />
-    <meta name="keywords" content="Helioviewer, hv, jpeg 2000, jp2, solar image viewer, sun, solar, heliosphere, solar physics, viewer, visualization, space, astronomy, SOHO, EIT, LASCO, SDO, MDI, coronagraph, " />
-    <?php if ($config["disable_cache"]) echo "<meta http-equiv=\"Cache-Control\" content=\"No-Cache\" />\n"; ?>
-
     <!-- YUI CSS Reset -->
     <link rel="stylesheet" href="lib/yui-2.8.2r1/reset-fonts.css" />
 
-    <!-- Kaltura HTML5 video player -->
-    <!-- <script type="text/javascript" src="http://html5.kaltura.org/js"></script> --> 
-
-    <!-- jQuery -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js" type="text/javascript"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/jquery-ui.min.js" type="text/javascript"></script>
-    <script src="lib/jquery.class/jquery.class.js" type="text/javascript"></script>
-    <!--<script src="lib/jquery.qtip-2.0-r282/jquery.qtip.js" type="text/javascript"></script>
-    <script src="lib/jquery.qtip-2.0-r282/jquery.qtip.tips.js" type="text/javascript"></script>-->
-    <script src="lib/jquery.qtip-1.0-r54/jquery.qtip-1.0.min.js" type="text/javascript"></script>
-
-    <link rel="stylesheet" href="lib/jquery.ui-1.8/css/dot-luv-modified/jquery-ui-1.8.custom.css" />    
-
-    <!-- Mousewheel support -->
-    <script src="lib/jquery.mousewheel.3.0.2/jquery.mousewheel.min.js" type="text/javascript"></script>
-
-    <!-- jGrowl -->
-    <script src="lib/jquery.jgrowl/jquery.jgrowl_minimized.js" type="text/javascript"></script>
+    <!-- Plugin Styles -->
+    <link rel="stylesheet" href="lib/jquery.ui-1.8/css/dot-luv-modified/jquery-ui-1.8.custom.css" />  
     <link rel="stylesheet" href="lib/jquery.jgrowl/jquery.jgrowl.css" />
-
-    <!-- imgAreaSelect jQuery plugin -->
-    <script src="lib/jquery.imgareaselect-0.9.2/scripts/jquery.imgareaselect.pack.js" type="text/javascript"></script>
     <link rel="stylesheet" href="lib/jquery.imgareaselect-0.9.2/css/imgareaselect-default.css" />
-    
-    <!-- jsTree -->
-    <!--<script src="lib/jquery.jsTree-1.0rc/jquery.jstree.js" type="text/javascript"></script>-->
-
-    <!-- CookieJar -->
-    <script type="text/javascript" src="lib/jquery.json-2.2/jquery.json-2.2.min.js"></script>
-    <script type="text/javascript" src="lib/jquery.cookie/jquery.cookie.js"></script>
-    <script type="text/javascript" src="lib/Cookiejar/jquery.cookiejar.pack.js"></script>
-
-    <!-- date.js -->
-    <script src="lib/date.js/date-en-US.js" type="text/javascript"></script>
-    
-    <!-- XML to JSON -->
-    <script src="lib/jquery.xml2json/jquery.xml2json.pack.js" type="text/javascript" language="javascript"></script>
-    
-    <!-- Simile -->
-    <!--<script src="http://static.simile.mit.edu/timeline/api-2.2.0/timeline-api.js" type="text/javascript"></script>-->
-
-    <!-- Pixastic -->
-    <!-- $("img.tile[src!=resources/images/transparent_512.gif]"); -->
-    <!--<script src="lib/pixastic/pixastic.custom.js" type="text/javascript"></script>-->
-
-    <!-- ShadowBox -->
     <link rel="stylesheet" href="lib/shadowbox-3.0.3/shadowbox.css">
-    <script type="text/javascript" src="lib/shadowbox-3.0.3/shadowbox.js"></script>
 
-    <!-- Helioviewer-Specific -->
+    <!-- Helioviewer-Specific Styles -->
     <?php
 
         if ($config['disable_cache']) {
@@ -84,41 +42,6 @@ if ((!file_exists($ini)) || (!$config = parse_ini_file($ini)))
             $signature = $config['build_num'];
         }
         $version = "rev=$signature";
-
-        // JavaScript
-        if ($config["compress_js"]) {
-        	$compressed = "build/helioviewer.min.js";
-        	if (!file_exists($compressed)) {
-        	   $error = "<div style='position: absolute; width: 100%; text-align: center; top: 40%; font-size: 14px;'>
-        	             <img src='resources/images/logos/about.png' alt='helioviewer logo'></img><br>
-        	             <b>Configuration:</b> Unable to find compressed JavaScript files.
-        	             If you haven't already, use Apache Ant with the included build.xml file to generate 
-                         compressed files.</div></body></html>";
-        	   die($error);
-        	}
-
-        	echo "<script src=\"$compressed?$version\" type=\"text/javascript\"></script>\n\t";
-        }
-        else {
-        	$js = array("Utility/Config.js", "UIController.js", "Utility/HelperFunctions.js", "UI/IconPicker.js", 
-                        "Tiling/Layer/Layer.js", "Tiling/Layer/TileLoader.js", "Tiling/Layer/TileLayer.js", 
-                        "Tiling/Layer/HelioviewerTileLayer.js", "UI/TreeSelect.js", "UI/ImageSelectTool.js",  
-                        "Utility/KeyboardManager.js", "Tiling/Manager/LayerManager.js", "Events/EventManager.js", 
-        	            "Tiling/Manager/TileLayerManager.js", "Tiling/Manager/HelioviewerTileLayerManager.js", 
-        	            "Events/EventType.js", "Events/EventFeatureRecognitionMethod.js",
-        	            "Media/History/History.js", "Media/History/MovieHistory.js", "Media/History/ScreenshotHistory.js",
-                        "Media/Builder/MediaBuilder.js", "UI/MessageConsole.js", "Media/MediaHistoryBar.js",
-        	            "Media/Media.js", "Media/Screenshot.js", "Media/Movie.js", "Media/Builder/MovieBuilder.js",  
-                        "Media/Builder/ScreenshotBuilder.js", "Image/JP2Image.js", "Tiling/Manager/TileLayerAccordion.js",  
-                        "UI/TimeControls.js", "UI/TooltipHelper.js", "Utility/SettingsLoader.js", "Utility/UserSettings.js", 
-                        "Utility/FullscreenControl.js", "Viewport/Helper/MouseCoordinates.js", "Viewport/Viewport.js", 
-                        "Viewport/Helper/HelioviewerMouseCoordinates.js", "Viewport/Helper/SandboxHelper.js",
-        	            "Viewport/Helper/ViewportMovementHelper.js", "Viewport/HelioviewerViewport.js", 
-        	            "Viewport/ViewportController.js", "Helioviewer.js", "UI/ZoomControls.js", 
-        	            "Utility/InputValidator.js", "UI/jquery.ui.dynaccordion.js");
-            foreach($js as $file)
-                printf("<script src=\"src/%s?$version\" type=\"text/javascript\"></script>\n\t", $file);
-        }
 
         // CSS
         if ($config["compress_css"]) {
@@ -133,52 +56,6 @@ if ((!file_exists($ini)) || (!$config = parse_ini_file($ini)))
 
     <!-- Theme Modifications -->
     <link rel="stylesheet" href="resources/css/dot-luv.css">
-
-    <script type="text/javascript">
-        var serverSettings, settingsJSON, urlSettings;
-
-        Shadowbox.init({
-            overlayOpacity: 0.5,
-            troubleElements: ["object", "embed"]
-        });
-
-        $(function () {
-            if ($.browser.msie && ($.browser.version < 8)) {
-                CFInstall.check({mode: "overlay"});
-            }
-
-            <?php
-                printf("settingsJSON = %s;\n", json_encode($config));
-
-                // Settings specified via URL parameters
-                $urlSettings = array();
-
-                //API Example: helioviewer.org/?date=2003-10-05T00:00:00Z&imageScale=2.4&imageLayers=[SOHO,AIA,AIA,171,1,70],[SOHO,LASCO,C2,white light,0,100]
-                if (isset($_GET['imageLayers'])) {
-                    $imageLayersString = ($_GET['imageLayers'][0] == "[") ? substr($_GET['imageLayers'],1,-1) : $_GET['imageLayers'];
-                    $imageLayers = preg_split("/\],\[/", $imageLayersString);
-                    $urlSettings['imageLayers'] = $imageLayers;
-                }
-
-                if (isset($_GET['date']))
-                    $urlSettings['date'] = $_GET['date'];
-
-                if (isset($_GET['imageScale']))
-                    $urlSettings['imageScale'] = $_GET['imageScale'];
-
-                // Convert to JSON
-                printf("\t\turlSettings = %s;\n", json_encode($urlSettings));
-            ?>
-            serverSettings = new Config(settingsJSON).toArray();
-
-            // Catch any exceptions that arise during execution
-            try {
-                helioviewer = new Helioviewer(urlSettings, serverSettings);
-            } catch (e) {
-                console.log("Exception: " + e.description);
-            }
-        });
-    </script>
 
 </head>
 <body>
@@ -360,5 +237,134 @@ if ((!file_exists($ini)) || (!$config = parse_ini_file($ini)))
 
 <!-- Usage Dialog -->
 <div id='usage-dialog'></div>
+
+<!-- Kaltura HTML5 video player -->
+<!-- <script type="text/javascript" src="http://html5.kaltura.org/js"></script> --> 
+
+<!-- jQuery -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js" type="text/javascript"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/jquery-ui.min.js" type="text/javascript"></script>
+<script src="lib/jquery.class/jquery.class.js" type="text/javascript"></script>
+<!--<script src="lib/jquery.qtip-2.0-r282/jquery.qtip.js" type="text/javascript"></script>
+<script src="lib/jquery.qtip-2.0-r282/jquery.qtip.tips.js" type="text/javascript"></script>-->
+<script src="lib/jquery.qtip-1.0-r54/jquery.qtip-1.0.min.js" type="text/javascript"></script>
+
+<!-- Mousewheel support -->
+<script src="lib/jquery.mousewheel.3.0.2/jquery.mousewheel.min.js" type="text/javascript"></script>
+
+<!-- jGrowl -->
+<script src="lib/jquery.jgrowl/jquery.jgrowl_minimized.js" type="text/javascript"></script>
+
+<!-- imgAreaSelect jQuery plugin -->
+<script src="lib/jquery.imgareaselect-0.9.2/scripts/jquery.imgareaselect.pack.js" type="text/javascript"></script>
+
+<!-- jsTree -->
+<!--<script src="lib/jquery.jsTree-1.0rc/jquery.jstree.js" type="text/javascript"></script>-->
+
+<!-- CookieJar -->
+<script type="text/javascript" src="lib/jquery.json-2.2/jquery.json-2.2.min.js"></script>
+<script type="text/javascript" src="lib/jquery.cookie/jquery.cookie.js"></script>
+<script type="text/javascript" src="lib/Cookiejar/jquery.cookiejar.pack.js"></script>
+
+<!-- date.js -->
+<script src="lib/date.js/date-en-US.js" type="text/javascript"></script>
+
+<!-- XML to JSON -->
+<script src="lib/jquery.xml2json/jquery.xml2json.pack.js" type="text/javascript" language="javascript"></script>
+
+<!-- Simile -->
+<!--<script src="http://static.simile.mit.edu/timeline/api-2.2.0/timeline-api.js" type="text/javascript"></script>-->
+
+<!-- Pixastic -->
+<!-- $("img.tile[src!=resources/images/transparent_512.gif]"); -->
+<!--<script src="lib/pixastic/pixastic.custom.js" type="text/javascript"></script>-->
+
+<!-- ShadowBox -->
+<script type="text/javascript" src="lib/shadowbox-3.0.3/shadowbox.js"></script>
+
+<!-- Helioviewer JavaScript -->
+<?php 
+    // JavaScript
+    if ($config["compress_js"]) {
+        $compressed = "build/helioviewer.min.js";
+        if (!file_exists($compressed)) {
+           $error = "<div style='position: absolute; width: 100%; text-align: center; top: 40%; font-size: 14px;'>
+                     <img src='resources/images/logos/about.png' alt='helioviewer logo'></img><br>
+                     <b>Configuration:</b> Unable to find compressed JavaScript files.
+                     If you haven't already, use Apache Ant with the included build.xml file to generate 
+                     compressed files.</div></body></html>";
+           die($error);
+        }
+    
+        echo "<script src=\"$compressed?$version\" type=\"text/javascript\"></script>\n\t";
+    }
+    else {
+        $js = array("Utility/Config.js", "UIController.js", "Utility/HelperFunctions.js", "UI/IconPicker.js", 
+                    "Tiling/Layer/Layer.js", "Tiling/Layer/TileLoader.js", "Tiling/Layer/TileLayer.js", 
+                    "Tiling/Layer/HelioviewerTileLayer.js", "UI/TreeSelect.js", "UI/ImageSelectTool.js",  
+                    "Utility/KeyboardManager.js", "Tiling/Manager/LayerManager.js", "Events/EventManager.js", 
+                    "Tiling/Manager/TileLayerManager.js", "Tiling/Manager/HelioviewerTileLayerManager.js", 
+                    "Events/EventType.js", "Events/EventFeatureRecognitionMethod.js",
+                    "Media/History/History.js", "Media/History/MovieHistory.js", "Media/History/ScreenshotHistory.js",
+                    "Media/Builder/MediaBuilder.js", "UI/MessageConsole.js", "Media/MediaHistoryBar.js",
+                    "Media/Media.js", "Media/Screenshot.js", "Media/Movie.js", "Media/Builder/MovieBuilder.js",  
+                    "Media/Builder/ScreenshotBuilder.js", "Image/JP2Image.js", "Tiling/Manager/TileLayerAccordion.js",  
+                    "UI/TimeControls.js", "UI/TooltipHelper.js", "Utility/SettingsLoader.js", "Utility/UserSettings.js", 
+                    "Utility/FullscreenControl.js", "Viewport/Helper/MouseCoordinates.js", "Viewport/Viewport.js", 
+                    "Viewport/Helper/HelioviewerMouseCoordinates.js", "Viewport/Helper/SandboxHelper.js",
+                    "Viewport/Helper/ViewportMovementHelper.js", "Viewport/HelioviewerViewport.js", 
+                    "Viewport/ViewportController.js", "Helioviewer.js", "UI/ZoomControls.js", 
+                    "Utility/InputValidator.js", "UI/jquery.ui.dynaccordion.js");
+        foreach($js as $file)
+            printf("<script src=\"src/%s?$version\" type=\"text/javascript\"></script>\n\t", $file);
+    }
+?>
+
+<script type="text/javascript">
+    var serverSettings, settingsJSON, urlSettings;
+
+    Shadowbox.init({
+        overlayOpacity: 0.5,
+        troubleElements: ["object", "embed"]
+    });
+
+    $(function () {
+        if ($.browser.msie && ($.browser.version < 8)) {
+            CFInstall.check({mode: "overlay"});
+        }
+
+        <?php
+            printf("settingsJSON = %s;\n", json_encode($config));
+
+            // Settings specified via URL parameters
+            $urlSettings = array();
+
+            //API Example: helioviewer.org/?date=2003-10-05T00:00:00Z&imageScale=2.4&imageLayers=[SOHO,AIA,AIA,171,1,70],[SOHO,LASCO,C2,white light,0,100]
+            if (isset($_GET['imageLayers'])) {
+                $imageLayersString = ($_GET['imageLayers'][0] == "[") ? substr($_GET['imageLayers'],1,-1) : $_GET['imageLayers'];
+                $imageLayers = preg_split("/\],\[/", $imageLayersString);
+                $urlSettings['imageLayers'] = $imageLayers;
+            }
+
+            if (isset($_GET['date']))
+                $urlSettings['date'] = $_GET['date'];
+
+            if (isset($_GET['imageScale']))
+                $urlSettings['imageScale'] = $_GET['imageScale'];
+
+            // Convert to JSON
+            printf("\t\turlSettings = %s;\n", json_encode($urlSettings));
+        ?>
+        serverSettings = new Config(settingsJSON).toArray();
+
+        // Catch any exceptions that arise during execution
+        try {
+            helioviewer = new Helioviewer(urlSettings, serverSettings);
+        } catch (e) {
+            console.log("Exception: " + e.description);
+        }
+    });
+</script>
+
 </body>
 </html>
