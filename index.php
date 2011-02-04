266cee3b9e27956918a2eb9fc1a0a3335d300483
Keith Hughitt <keith.hughitt@nasa.gov>
2011-02-04 13:42:24 -0500

    Fixed Bug #662902: update to jQuery 1.5. Image information dialog button now uses info icon instead of text.

--- index.php
+++ index.php
@@ -285,7 +285,7 @@ unset($config['youtube_developer_key'], $config['enable_statistics_collection'])
 </div>
 
 <!-- jQuery -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js" type="text/javascript"></script>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/jquery-ui.min.js" type="text/javascript"></script>
 <script src="lib/jquery.class/jquery.class.min.js" type="text/javascript"></script>
