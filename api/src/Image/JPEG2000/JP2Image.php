0d5bd3c561c587dd955dcc9046bfb7174052c902
Keith Hughitt <keith.hughitt@nasa.gov>
2011-03-08 14:23:12 -0500

    Updated JP2Image class to improve support for Kakadu 6.4.1

--- api/src/Image/JPEG2000/JP2Image.php
+++ api/src/Image/JPEG2000/JP2Image.php
@@ -110,7 +110,7 @@ class Image_JPEG2000_JP2Image
         // Execute the command
 
         $result = exec(HV_PATH_CMD . escapeshellcmd($cmd), $out, $ret);
-        if (($ret != 0) || (sizeof($out) > 5)) {
+        if (($ret != 0) || (sizeof($out) > 6)) {
             $msg = "Error extracting JPEG 2000 subfield region!\n\tCommand: \"" . escapeshellcmd($cmd) . "\".\n\tResult: $result";
             throw new Exception($msg);
         }
