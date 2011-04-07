3ad1aa096bb4da901f4d3126c2cf6cb36b0bc075
Keith Hughitt <keith.hughitt@nasa.gov>
2011-04-07 16:04:40 -0400

    Created a MediaManagerUI class to abstract functionality common to the screenshot and movie managers

--- src/Media/ScreenshotManagerUI.js
+++ src/Media/ScreenshotManagerUI.js
@@ -6,7 +6,7 @@
 bitwise: true, regexp: false, strict: true, newcap: true, immed: true, maxlen: 120, sub: true */
 /*global Class, $, setTimeout, window, Media, extractLayerName, addIconHoverEventListener */
 "use strict";
-var ScreenshotManagerUI = Class.extend(
+var ScreenshotManagerUI = MediaManagerUI.extend(
     /** @lends ScreenshotManagerUI */
     {
     /**
@@ -15,73 +15,11 @@ var ScreenshotManagerUI = Class.extend(
      * 
      * @param {ScreenshotManager} model ScreenshotManager instance
      */    
-    init: function (model) {
-        this._screenshots = model;
-        
-        this._btn             = $("#screenshot-button");
-        this._container       = $("#screenshot-manager-container");
-        this._history         = $("#screenshot-history");
-        this._buildBtns       = $("#screenshot-manager-build-btns");
-        this._fullViewportBtn = $("#screenshot-manager-full-viewport");
-        this._selectAreaBtn   = $("#screenshot-manager-select-area");
-        this._historyTitle    = $("#screenshot-history-title");
-        this._clearBtn        = $("#screenshot-clear-history-button");
+    init: function (screenshotManager) {
+        this._manager = screenshotManager;
+        this._super("screenshot");
 
         this._initEvents();
-        this._loadScreenshots();
-    },
-    
-    /**
-     * Hides the screenshot manager
-     */
-    hide: function () {
-        this._container.hide();
-    },
-    
-    /**
-     * Shows the screenshot manager
-     */
-    show: function () {
-        this._refresh();
-        this._container.show();
-    },
-    
-    /**
-     * Toggles the visibility of the screenshot manager
-     */
-    toggle: function () {
-        if (this._container.is(":visible")) {
-            this.hide();
-        } else {
-            this.show();
-        }
-    },
-    
-    /**
-     * Adds a single screenshot entry to the history
-     */
-    _addScreenshot: function (screenshot) {
-        var id, html, name = screenshot.name;
-
-        if (name.length > 16) {
-            name = name.slice(0, 16) + "...";
-        }
-        
-        html = "<div id='" + screenshot.id + "' class='history-entry'>" +
-               "<div class='text-btn' style='float:left'>" + name + 
-               "</div>" +
-               "<div class='time-elapsed' style='float:right; font-size: 8pt; font-style:italic;'></div><br /><br />" +
-               "</div>";
-        
-        this._history.prepend(html);
-
-        if (this._history.find(".history-entry").length > 12) {
-            id = this._history.find(".history-entry").last().attr('id');
-            this._removeScreenshot(id);
-            this._screenshots.remove(id);
-        }
-        
-        this._historyTitle.show();
     },
     
     /**
@@ -118,15 +56,8 @@ var ScreenshotManagerUI = Class.extend(
      */
     _initEvents: function () {
         var self = this;
-        
-        addIconHoverEventListener(this._fullViewportBtn);
-        addIconHoverEventListener(this._selectAreaBtn);
-        addIconHoverEventListener(this._clearBtn);
-
-        this._btn.click(function () {
-           self.toggle();
-           $(".jGrowl-notification .close").click(); // Close any open jGrowl notifications
-        });
+       
+        this._super();
         
         this._fullViewportBtn.click(function () {
             self.hide();
@@ -137,65 +68,22 @@ var ScreenshotManagerUI = Class.extend(
             self.hide();
             $(document).trigger("enable-select-tool", $.proxy(self._takeScreenshot, self));
         });
-        
-        this._clearBtn.click(function () {
-            $.each(self._screenshots.toArray(), function (i, screenshot) {
-                self._removeScreenshot(screenshot.id);
-            })
-            self._screenshots.empty();
-        });
     },
 
-    /**
-     * Creates HTML for screenshot history entries
-     */
-    _loadScreenshots: function () {
-        var self = this;
-
-        $.each(this._screenshots.toArray(), function (i, screenshot) {
-            self._addScreenshot(screenshot);
-        });
-    },
-    
-    /**
-     * Refreshes status information for screenshots in the history
-     */
-    _refresh: function () {
-        $.each(this._screenshots.toArray(), function (i, screenshot) {
-            var elapsedTime = Date.parseUTCDate(screenshot.dateRequested).getElapsedTime()
-            $("#" + screenshot.id).find(".time-elapsed").html(elapsedTime);
-        });
-    },
-    
-    /**
-     * Removes a single screenshot from the history.
-     * 
-     * @param {String} Identifier of the screenshot to be removed
-     */
-    _removeScreenshot: function (id) {
-        $("#" + id).unbind().remove();
-        
-        if (this._history.find(".history-entry").length === 0) {
-            this._historyTitle.hide();
-        }
-    },
-    
     /**
      * @description Gathers all necessary information to generate a screenshot, and then displays the
      *              image when it is ready.
      * @param {Object} roi Region of interest to use in place of the current viewport roi
      */
     _takeScreenshot: function (roi) {
-        var params, imageScale, date, layers, servers, server, screenshot, self = this; 
+        var params, imageScale, layers, server, screenshot, self = this; 
         
         if (typeof roi === "undefined") {
             roi = helioviewer.getViewportRegionOfInterest();
         }
 
-        date       = helioviewer.getDate().toISOString();
         imageScale = helioviewer.getImageScale();
         layers     = helioviewer.getLayers();
-        servers    = helioviewer.getServers();
 
         // Make sure selection region and number of layers are acceptible
         if (!this._validateRequest(roi, layers)) {
@@ -207,12 +95,12 @@ var ScreenshotManagerUI = Class.extend(
             dateRequested : new Date().toISOString(),
             imageScale    : imageScale,
             layers        : layers,
-            date          : date,
+            date          : helioviewer.getDate().toISOString(),
             display       : false
         }, this._toArcsecCoords(roi, imageScale));
         
         // Choose server to send request to
-        server = Math.floor(Math.random() * (servers.length));
+        server = Math.floor(Math.random() * (helioviewer.getServers().length));
         if (server > 0) {
             params.server = server;
         }
@@ -224,27 +112,13 @@ var ScreenshotManagerUI = Class.extend(
                 return;
             }
             
-            screenshot = self._screenshots.add(
+            screenshot = self._manager.add(
                 response.id, params.imageScale, params.layers, params.dateRequested, params.date, 
                 params.x1, params.x2, params.y1, params.y2
             );
-            self._addScreenshot(screenshot);
+            self._addItem(screenshot);
             self._displayDownloadNotification(screenshot.id);
         });
-    },
-    
-    /**
-     * Translates viewport coordinates into arcseconds
-     */
-    _toArcsecCoords: function (pixels, scale) {
-        var coordinates = {
-            x1: pixels.left,
-            x2: pixels.right,
-            y1: pixels.top,
-            y2: pixels.bottom
-        };
-        
-        return pixelsToArcseconds(coordinates, scale);
     },
     
     /**
