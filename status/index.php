3e8190836e929ace4d7e2506db434934e5336726
Keith Hughitt <keith.hughitt@nasa.gov>
2011-04-26 16:38:35 -0400

    Updated data monitor page to include continous coloring for status icons and collapsible instrument tables

--- status/index.php
+++ status/index.php
@@ -14,8 +14,9 @@
         #headerText {display: inline-block; vertical-align: super; font-family: arial,helvetica,liberation sans,bitstream vera sans,freesans,clean,sans-serif; font-size:2.4em;}
         #currentTime {font-size:0.8em; font-style: italic;}
         #statuses {margin-left: auto; margin-right: auto; width:65%; min-width:480px; text-align: left; font-size:0.9em;}
+        .instrument {font-weight: bold; cursor: pointer;}
+        .datasource {display: none; background-color: #F9F9F9;}
         .status-icon {width: 18px; height: 18px; mask: url(mask.svg#mask); -webkit-mask-box-image: url(mask.svg);}
-        
     </style>
 </head>
 <body>
@@ -27,10 +28,10 @@
     </div>
 
     <table id='statuses'>
-    <tr style='font-weight:bold;'>
-        <td width='100'>Datasource</td>
-        <td width='100'>Latest Image</td>
-        <td width='50' align='center'>Status</td>
+    <tr style='font-weight:bold; background: #444; color: #fff;'>
+        <th width='100'>Datasource</th>
+        <th width='100'>Latest Image</th>
+        <th width='50' align='center'>Status</th>
     </tr>    
     <?php
         include_once "../api/src/Database/DbConnection.php";
@@ -38,12 +39,9 @@
         include_once "../api/src/Config.php";
 
        /**
-        * Returns a CSS RGB triplet ranging from green (close to requested time) to yellow (some deviation from requested
-        * time) to red (requested time differs strongly from actual time).
-        * 
-        * The weight should range from:
-        *   0 = green
-        *   1 = red
+        * Returns a CSS RGB triplet ranging from green (close to requested time)
+        * to yellow (some deviation from requested time) to red (requested 
+        * time differs strongly from actual time).
         * 
         * @param float $weight  Numeric ranging from 0.0 (green) to 1.0 (red)
         * @param int   $rOffset Offset to add to red value
@@ -56,10 +54,38 @@
             $blue  = $bOffset;
 
             return sprintf("rgb(%d,%d,%d)", $red, $green, $blue);
-        } 
+        }
+        
+        /**
+         * Computes the weight to use when choosing a color based off
+         * the image cadence (min) and some reasonable lag time (max)
+         * 
+         * weight = e^[-(t_elapsed - t_min) / t_max]
+         * 
+         * @param int    $elapsed Number of elapsed seconds
+         * @param string $inst    Name of the instrument
+         * 
+         * @return float a value from 0 (green) to 1 (red)
+         */
+        function computeStatusWeight($elapsed, $inst) {
+            // Default values
+            $min = 3600;  // 1 hr
+            $max = 86400; // 24 hrs
+            
+            if ($inst == "EIT") {
+                $min = 7 * 3600;
+                $max = 48 * 3600;                
+            } else if ($inst == "LASCO") {
+                $min = 6 * 3600;
+                $max = 48 * 3600;   
+            } else if ($inst == "HMI") {
+                $max = 12 * 3600;
+            }
+
+            return exp(- min(($elapsed - $min), 0) / $max);
+        }
         
         $config = new Config("../settings/Config.ini");
-        
         
         // Current time
         $now = time();
@@ -67,37 +93,113 @@
         $db = new Database_DbConnection();
         $imgIndex = new Database_ImgIndex();
         
-        $datasources = $db->query("SELECT * FROM datasources ORDER BY name");
+        // Get a list of the datasources grouped by instrument
+        $result = $db->query("SELECT * FROM instruments ORDER BY name");
+        
+        $instruments = array();
 
-        // Add an entry to the table for each data source
-        while($ds = mysqli_fetch_assoc($datasources)) {
-            // Get date of most recent image
-            $date = $imgIndex->getNewestImage($ds['id']);
-            
-            $icon = "<div class='status-icon' style='background-color:%s' />";
-
-            // Determine status from 1 (out of date) to 4 (recent)
-            $elapsed = $now - strtotime($date);
-            
-            if ($elapsed < 60 * 60) {
-                $icon = sprintf($icon, chooseTimeStampColor(0));
-            } else if ($elapsed < 2 * 60 * 60) {
-                $icon = sprintf($icon, chooseTimeStampColor(0.33));
-            } else if ($elapsed < 3 * 60 * 60) {
-                $icon = sprintf($icon, chooseTimeStampColor(0.66));
-            } else {
-                $icon = sprintf($icon, chooseTimeStampColor(1));
+        while($instrument = mysqli_fetch_assoc($result)) {
+            $instruments[$instrument['name']] = array();
+            $datasources = $db->query(sprintf("SELECT * FROM datasources WHERE instrumentId=%d ORDER BY name", $instrument['id']));
+            while($ds = mysqli_fetch_assoc($datasources)) {
+                array_push($instruments[$instrument['name']], $ds);
             }
+        }
+        
+        $tableRow = "<tr class='%s'><td>%s</td><td>%s</td><td align='center'>%s</td></tr>";
+        
+        // Create table of datasource statuses
+        foreach($instruments as $name => $datasources) {
+            $oldest = array(
+                "weight"      => 0,
+                "datetime"    => null,
+                "icon"        => null
+            );
+            $maxElapsed = 0;
+            $oldestDate = null;
+            $subTableHTML = "";
             
-            //$dateStr = getdate(strtotime($date));
-            $timestamp = strtotime($date);
-            $datetime = new DateTime("@$timestamp");
+            // Create table row for a single datasource
+            foreach($datasources as $ds) {
+                
+                // Determine weight of status icon
+                $date = $imgIndex->getNewestImage($ds['id']);
+                $elapsed = $now - strtotime($date);
+                $weight = computeStatusWeight($elapsed, $name);
+                
+                // Create status icon
+                $icon = sprintf("<div class='status-icon' style='background-color:%s' />", chooseTimeStampColor($weight));
+                
+                // Convert to human-readable date
+                $timestamp = strtotime($date);
+                $datetime = new DateTime("@$timestamp");
+                
+                // CSS classes for row
+                $classes = "datasource $name";
 
-            printf("<tr><td>%s</td><td>%s</td><td align='center'>%s</td></tr>", 
-                   $ds['name'], $datetime->format('M j H:i:s'), $icon);
+                // create HTML for subtable row
+                $subTableHTML .= sprintf($tableRow, $classes, "&nbsp;&nbsp;&nbsp;&nbsp;" . $ds['name'], $datetime->format('M j H:i:s'), $icon);
+                
+                // If the elapsed time is greater than previous max store it
+                if ($weight > $oldest['weight']) {
+                    $oldest = array(
+                        'weight'   => $weight,
+                        'datetime' => $datetime,
+                        'icon'     => $icon
+                    );
+                }
+            }
+
+            // Ignore datasources with no data
+            if ($oldest['datetime']) {
+                $datetime = $oldest['datetime'];
+                printf($tableRow, "instrument", $name, $datetime->format('M j H:i:s'), $oldest['icon']);
+                print($subTableHTML);
+            }
         }
     ?>
     </table>
+    
+    <!-- Legend -->
+    <!--
+    <div id='legend' style='border: 1px solid black; margin: 0 auto; padding: 10px;'>
+        <table>
+            <tr>
+                <th>Icon</th>
+                <th>Status</th>
+            </tr>
+            <tr>
+                <td><div class='status-icon' style='background-color:<?php echo chooseTimeStampColor(0);?>'></div></td>
+                <td>:)</td>
+            </tr>
+            <tr>
+                <td><div class='status-icon' style='background-color:<?php echo chooseTimeStampColor(0.33);?>'></div></td>
+                <td>:|</td>
+            </tr>
+            <tr>
+                <td><div class='status-icon' style='background-color:<?php echo chooseTimeStampColor(0.66);?>'></div></td>
+                <td>:(</td>
+            </tr>
+            <tr>
+                <td><div class='status-icon' style='background-color:<?php echo chooseTimeStampColor(1);?>'></div></td>
+                <td>:*(</td>
+            </tr>
+        </table>
     </div>
+    </div> -->
+
+    <!-- jQuery -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script>
+    
+    <!-- JavaScript -->
+    <script type='text/javascript'>
+    $(function() {
+        $(".instrument").click(function (e) {
+            var inst = $($(this).find("td")[0]).text();
+
+            $(".datasource." + inst).toggle();
+        });
+    });    
+    </script>
 </body>
 </html>
