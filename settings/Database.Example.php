e21f1fde416d55d9f60333ffe7243d8d0cbbb10b
Keith Hughitt <keith.hughitt@nasa.gov>
2011-05-26 11:36:38 -0400

    Converted Database.php to a more general Private.php. Moved YouTube dev key from Config.ini to Private.php and added a HV_MOVIE_ID_PASS key to use when creating unique movie ids.

--- settings/Database.Example.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-* Helioviewer Database Configuration
-*/
-define("HV_DB_HOST", "localhost");
-define("HV_DB_NAME", "helioviewer");
-define("HV_DB_USER", "helioviewer");
-define("HV_DB_PASS", "helioviewer");
-?>
