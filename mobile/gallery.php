02ed5a2a911a95b9327bc24ed351d093f16d19e0
Keith Hughitt <keith.hughitt@nasa.gov>
2011-06-20 11:15:02 -0400

    Single page mobile video gallery now working.

--- mobile/gallery.php
+++ mobile/gallery.php
@@ -2,19 +2,23 @@
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <title>Helioviewer.org - Video Gallery</title>
+    <title>Helioviewer.org - Recent Videos</title>
     <link rel="stylesheet" href="http://code.jquery.com/mobile/latest/jquery.mobile.min.css" />
     <script type="text/javascript" src="http://code.jquery.com/jquery-1.5.2.min.js"></script>
-    <script type="text/javascript" src="http://code.jquery.com/mobile/latest/jquery.mobile.min.js"></script>
+    <script type="text/javascript" src="http://code.jquery.com/mobile/latest/jquery.mobile.js"></script>
 </head>
 <body>
     <div id="main" data-role="page"> 
-    <div data-role="header" data-theme="b"><h1>Helioviewer.org - Video Gallery</h1></div> 
+    <div data-role="header" data-theme="a">
+        <h1>Helioviewer.org - Recent Videos</h1>
+    </div> 
     <div data-role="content">
         <ul id="videos" data-role="listview">
         </ul>
     </div> 
-    <div data-role="footer"></div> 
+    <div data-role="footer">
+        <!--<a href="#page=2" data-icon="arrow-r" class="ui-btn-right">Next</a>-->
+    </div> 
     </div>
     <script type="text/javascript" src="../src/Utility/HelperFunctions.js"></script>
     <script type="text/javascript">
@@ -33,9 +37,8 @@
                     
                     date = new Date.parseUTCDate(video.published).getElapsedTime() + " ago";
                     
-                    html = "<li><a href='" + video.url + "'><img src='" + 
+                    html = "<li><a href='" + video.url + "' target='_blank'><img style='margin-top: 10px;' src='" + 
                            video.thumbnails.small + "' /><h3>" + date + "</h3>" +
-                           "<p>description</p>" +
                            "</a></li>";
                     $videos.append(html);
                 });
