13a637c0535094c39aa5454fbfa6c528258c6b47
Keith Hughitt <keith.hughitt@nasa.gov>
2012-03-09 10:07:38 -0500

    Installer PEP8 cleanup

--- install/helioviewer/jp2.py
+++ install/helioviewer/jp2.py
@@ -15,15 +15,11 @@ def find_images(path):
     the available images.
     '''
     images = []
-
-    for child in os.listdir(path):
-        node = os.path.join(path, child)
-        if os.path.isdir(node):
-            newImgs = find_images(node)
-            images.extend(newImgs)
-        else:
-            if node[-3:] == "jp2":
-                images.append(node)
+    
+    for root, dirs, files in os.walk(path):
+        for file_ in files:
+            if file_.endswith('.jp2'):
+                images.append(file_)
 
     return images
 
@@ -47,13 +43,13 @@ def parse_header(img):
     detector = get_element_value(fits, "DETECTOR")
     instrume = get_element_value(fits, "INSTRUME")
     
-    if instrume and instrume[0:3] == 'AIA':
+    if instrume and instrume.startswith('AIA'):
         datatype = "aia"
-    elif instrume and instrume[0:3] == 'HMI':
+    elif instrume and instrume.startswith('HMI'):
         datatype = "hmi"
     elif detector == 'EUVI':
         datatype = "euvi"
-    elif detector and detector[0:3] == "COR":
+    elif detector and detector.startswith("COR"):
         datatype = "cor"
     elif instrume == 'EIT':
         datatype = "eit"
@@ -204,7 +200,7 @@ def read_xmlbox(file, root):
     # 2010/04/12 TEMP Work-around for AIA invalid XML
     return xml.replace("&", "&amp;")
 
-def process_jp2_images (images, rootdir, cursor, mysql, stepFxn=None):
+def process_jp2_images (images, rootdir, cursor, mysql, step_function=None):
     '''Processes a collection of JPEG 2000 Images'''
     
     if mysql:
@@ -221,16 +217,16 @@ def process_jp2_images (images, rootdir, cursor, mysql, stepFxn=None):
     if len(images) >= __INSERTS_PER_QUERY__:
         for x in range(len(images) // __INSERTS_PER_QUERY__):
             insert_n_images(images, __INSERTS_PER_QUERY__, sources,
-                          rootdir, cursor, mysql, stepFxn)
+                          rootdir, cursor, mysql, step_function)
             
     # Update tree of known data-sources
     sources = get_datasources(cursor)
             
     # Process remaining images
-    insert_n_images(images, remainder, sources, rootdir, cursor, mysql, stepFxn)
+    insert_n_images(images, remainder, sources, rootdir, cursor, mysql, step_function)
 
     
-def insert_n_images(images, n, sources, rootdir, cursor, mysql, stepFxn=None):
+def insert_n_images(images, n, sources, rootdir, cursor, mysql, step_function=None):
     """Inserts multiple images into a database using a single query"""
     
     # 2011/06/27
@@ -273,8 +269,8 @@ def insert_n_images(images, n, sources, rootdir, cursor, mysql, stepFxn=None):
             query += "(NULL, '%s', '%s', '%s', %d)," % (path, filename, date, source['id'])
         
             # Progressbar
-            if stepFxn and (y + 1) % __STEP_FXN_THROTTLE__ is 0:
-                stepFxn(filename)
+            if step_function and (y + 1) % __STEP_FXN_THROTTLE__ is 0:
+                step_function(filename)
                 
     # Log any errors encountered
     if error:
