124c2376aaf3610188460ce470b050f850f12ab1
Keith Hughitt <keith.hughitt@nasa.gov>
2012-05-01 11:43:06 -0400

    HVPull now downloads files to a centralized folder before processing and moving into archive - this way we can normalize the filepaths.

--- install/helioviewer/downloader/downloader/urllib.py
+++ install/helioviewer/downloader/downloader/urllib.py
@@ -5,20 +5,14 @@ import threading
 from urllib2 import urlopen, Request # URLError, HTTPError
 
 class URLLibDownloader(threading.Thread):
-    def __init__(self, image_archive, working_dir, queue):
+    def __init__(self, incoming, queue):
         """Creates a new URLLibDownloader"""
         threading.Thread.__init__(self)
         
         self.shutdown_requested = False
-        
-        self.image_archive = image_archive
-        self.working_dir = working_dir
+        self.incoming = incoming
         self.queue = queue
-        self.quarantine = os.path.join(self.working_dir, 'quarantine')
-        
-        if not os.path.exists(self.quarantine):
-            os.makedirs(self.quarantine)
-            
+
     def stop(self):
         self.shutdown_requested = True
     
@@ -30,7 +24,7 @@ class URLLibDownloader(threading.Thread):
             # @TODO: compute path to download file to...
             
             # Location to save file to
-            filepath = os.path.join(self.image_archive, os.path.basename(url))
+            filepath = os.path.join(self.incoming, os.path.basename(url))
             
             # Create sub-directory if it does not already exist
             if not os.path.exists(os.path.dirname(filepath)):
