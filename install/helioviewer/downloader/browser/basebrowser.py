6f6a4319c11dabf29c25c4a40d483e7d3f573b9e
Keith Hughitt <keith.hughitt@nasa.gov>
2012-05-01 11:24:53 -0400

    Generalized remote directory handling for HVPull

--- install/helioviewer/downloader/browser/basebrowser.py
+++ install/helioviewer/downloader/browser/basebrowser.py
@@ -4,10 +4,8 @@ class BaseDataBrowser:
     def __init__(self, server):
         self.server = server
 
-    def get_directories(self, uri):
-        """Get a list of directories at the root of the dataprovider.  We
-        assume that these directories are in fact a list of instrument
-        nicknames."""
+    def get_directories(self, start_time, end_time):
+        """Gets a list of directories to be queried for the given time range"""
         return None
     
     def get_files(self, uri, extension):
--- install/helioviewer/downloader/servers/jsoc.py
+++ install/helioviewer/downloader/servers/jsoc.py
@@ -1,4 +1,5 @@
 """JSOC DataServer definition"""
+import os
 from helioviewer.downloader.servers import DataServer
 
 class JSOCDataServer(DataServer):
@@ -6,3 +7,15 @@ class JSOCDataServer(DataServer):
     def __init__(self):
         """Defines the root directory of where the data is kept at the JSOC."""
         DataServer.__init__(self, "http://jsoc.stanford.edu/data/aia/images", "JSOC")
+        
+    def compute_directories(self, start_date, end_date):
+        """Computes a list of remote directories expected to contain files"""
+        directories = []
+        
+        measurements = [94, 131, 171, 193, 211, 304, 335, 1600, 1700, 45000]
+        
+        for date in self.get_dates(start_date, end_date):
+            for meas in measurements:
+                directories.append(os.path.join(self.uri, date, str(meas)))
+                
+        return directories
