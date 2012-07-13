c3dba607c3a472f147d91e26cb344d67c63192e0
Keith Hughitt <keith.hughitt@nasa.gov>
2012-07-13 10:10:22 -0400

    Fixed bug #1024393: Helioviewer.org sometimes reports negative estimates for time to build a movie

--- api/scripts/launchpad_milestone_stats.py
+++ api/scripts/launchpad_milestone_stats.py
@@ -19,11 +19,16 @@ Resources:
 import sys
 import numpy
 import getpass
+import datetime
 from launchpadlib.launchpad import Launchpad
 
 def main():
+    # Check for milestone in input
+    if len(sys.argv) > 1:
+        milestone = sys.argv[1]
+    
     cachedir = "/home/%s/.launchpadlib/cache/" % getpass.getuser()
-
+    
     # Connect to Launchpad
     launchpad = Launchpad.login_anonymously('helioviewer', 'edge', cachedir)
     hv = launchpad.projects['helioviewer.org']
@@ -38,28 +43,52 @@ def main():
     
     milestone_name = milestones[selection - 1]['name']
     milestone = hv.getMilestone(name=milestone_name)
-
-    # Get bugs associated with milestone
-    bugs = milestone.searchTasks(status='Fix Released')
-
-    print("Total bugs: %d " % bugs.total_size)
-
-    # Committed
-    committed_deltas = []
-
-    for bug in bugs:
-        dt = bug.date_fix_committed - bug.date_created
-        committed_deltas.append(dt.total_seconds() / (60 * 60 * 24))
     
-    # Print min, max and average times to fix being committed
-    print("Average time to commit fix: %f days" % numpy.average(committed_deltas))
-    print("Min: %f days" % numpy.min(committed_deltas))
-    print("Max: %f days" % numpy.max(committed_deltas))
+    # Print Greeting
+    print("==========================================")
+    print("Helioviewer.org Milestone %s Statistics" % milestone_name)
+    print(str(datetime.datetime.now()))
+    print("==========================================\n")
+    print("BUGS:")
+
+    # Get tasks associated with milestone
+    num_bugs = 0
+    committed = []
+    
+    for status in ['New', 'Fix Committed', 'Fix Released']:
+        bugs = milestone.searchTasks(status=status)
+        num_bugs += bugs.total_size 
+        print("  %s: %d" % (status, bugs.total_size))
+        
+        # Committed bugs
+        if status != "New":
+            committed += bugs
+
+    print("-----------------")
+    print("Total: %d\n" % num_bugs)
+    
+    print("STATISTICS FOR COMMITTED BUGS:")
+    print_commit_stats(committed)
 
     # To show a histogram...
     # import matplotlib.pyplot as plt
     # plt.hist(committed_deltas, 30)
     # plt.show()
+    
+def print_commit_stats(bugs):
+    """Prints stats for bugs that have been marked as having a fix comitted
+    """
+    # Committed
+    deltas = []
+    
+    for bug in bugs:
+        dt = bug.date_fix_committed - bug.date_created
+        deltas.append(dt.total_seconds() / (60 * 60 * 24))
+
+    # Print min, max and average times to fix being committed
+    print("  Average time to commit fix: %f days" % numpy.average(deltas))
+    print("  Min: %f days" % numpy.min(deltas))
+    print("  Max: %f days" % numpy.max(deltas))
 
 if __name__ == '__main__':
     sys.exit(main())
