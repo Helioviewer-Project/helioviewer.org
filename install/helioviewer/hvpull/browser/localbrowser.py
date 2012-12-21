0e2cde12176d83e1156777875d163ca67dc05d8c
Jack Ireland <jack.ireland@nasa.gov>
2012-12-21 16:41:54 -0500

    further work on defining a 'local' method for obtaining files.

--- install/helioviewer/hvpull/browser/localbrowser.py
+++ install/helioviewer/hvpull/browser/localbrowser.py
@@ -9,11 +9,17 @@ class LocalDataBrowser(BaseDataBrowser):
     def get_directories(self,uri):
         """Get a list of directories at the passed uri"""
         return self.server.compute_directories(start_date, end_date)
-  
-    def get_files(self, uri, extension):
+
+    def get_files(self, location, extension):
         """Get all the files that end with specified extension at the uri"""
-        # ensure the location has the correct suffix
-        if os.path.exists(uri):
-            return [os.path.join(uri,f) for f in os.listdir(uri)]
+        files = filter(lambda uri: uri.endswith("." + extension),
+                       self._query(location))
+        return files
+    
+    def _query(self, location):
+        """Get a list of files and folders at the specified remote location"""
+        # query the local location for the list of files and subdirectories 
+        if os.path.exists(location):
+            return [os.path.join(location,f) for f in os.listdir(location)]
         else:
             return []
