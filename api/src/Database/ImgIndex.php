d9436fe335e9d6abb6f646481bad3df3fb257658
Jeff Stys <jstys@sesda3.com>
2014-03-31 11:09:10 -0400

    Added automatic re-generation of screenshots that no longer exist in the local cache, when requested via the 'downloadScreenshot' API.

--- api/src/Database/ImgIndex.php
+++ api/src/Database/ImgIndex.php
@@ -982,5 +982,30 @@ class Database_ImgIndex {
         return array_pop($row);
     }
 
+    /**
+     * Return from the database the parameters that were used to generate 
+     * a screenshot.
+     *
+     * @param int Unique screenshot identifier
+     *
+     * @return array Screenshot metadata
+     */
+    public function getScreenshotMetadata($screenshotId) {
+        $this->_dbConnect();
+
+        $sql = sprintf('SELECT *, AsText(regionOfInterest) as roi ' .
+            'FROM screenshots WHERE id=%d LIMIT 1;',
+             (int)$screenshotId
+        );
+        try {
+            $result = $this->_dbConnection->query($sql);
+        }
+        catch (Exception $e) {
+            return false;
+        }
+
+        return $result->fetch_array(MYSQLI_ASSOC);
+    }
+
 }
 ?>
