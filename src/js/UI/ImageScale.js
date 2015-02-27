6d31203c600dd3253693aa2c640386b30f0bd176
Jeff Stys <jstys@sesda3.com>
2015-02-27 16:58:57 -0500

    + Major refactoring of Earth Scale widget to work in new user-interface paradigm.

--- src/js/UI/ImageScale.js
+++ src/js/UI/ImageScale.js
@@ -23,9 +23,6 @@ var ImageScale = Class.extend(
         this._earthSunRadiusFraction = 6367.5 / 695500.;  // km
 
         this._initScale();
-
-        // Position and display ImageScale UI widget
-        this.display();
     },
 
 
@@ -42,22 +39,18 @@ var ImageScale = Class.extend(
         this.scale_container.draggable();
         this.scale_container.css({
             'position'    : 'absolute',
-            'bottom'      : '0',
             'z-index'     : '999',
             'width'       : '73px',
             'height'      : '56px',
             'background-color':'rgba(17,17,17,0.5)',
-            'border-top'  : '1px solid #333',
-            'border-right': '1px solid #333',
+            'border'      : '1px solid #888',
             'box-shadow'  : '0px 0px 5px black',
-            'cursor'      : 'move',
-            'display'     : 'none'
+            'cursor'      : 'move'
         });
         this.scale_container.attr('title','Click and drag to re-position scale indicator.');
 
         $('<div style="position:relative; height:12px;"><div id="earthLabel" style="color: white; background-color: #333; text-align: center; font-size: 10px; padding: 2px 0 2px 2px;">Earth Scale</div></div>').appendTo("#earth-container");
-
-        $('<div style="position:relative; width:72px; height:45px;"><img id="earthScale" src="resources/images/earth.png" style="width: '+this.earthDiameterInPixels+'px;height: '+this.earthDiameterInPixels+'px; position: absolute; left: '+(36-(this.earthDiameterInPixels/2))+'px; top: '+(23-(this.earthDiameterInPixels/2))+'px;" /></div>').appendTo("#earth-container");
+        $('<div style="position:relative; width:72px; height:45px;"><img id="earthScale" src="resources/images/earth.png" style="width: '+this.earthDiameterInPixels+'px; height: '+this.earthDiameterInPixels+'px; position: absolute; left: '+(36-(this.earthDiameterInPixels/2))+'px; top: '+(23-(this.earthDiameterInPixels/2))+'px;" /></div>').appendTo("#earth-container");
 
         this.scale_button    = $(document).find('#earth-button');
         this.scale_image     = this.scale_container.find('#earthScale');
@@ -69,10 +62,11 @@ var ImageScale = Class.extend(
         this.scale_container.bind('dblclick',  function () { return false; });
         this.scale_container.bind('click',     function () { return false; });
 
-        this.scale_container.bind('drag',     $.proxy(this.scaleContainerDrag, this));
         this.scale_container.bind('dragstop', $.proxy(this.scaleContainerDragStop, this));
 
         this.scale_button.bind('click', $.proxy(this.earthMinimize, this));
+
+        this.display();
     },
 
     earthRescale: function() {
@@ -94,96 +88,80 @@ var ImageScale = Class.extend(
     earthMinimize: function(event) {
         Helioviewer.userSettings.set("state.scale", false);
         Helioviewer.userSettings.set("state.scaleType", 'earth');
-        Helioviewer.userSettings.set("state.scaleX", 0);
-        Helioviewer.userSettings.set("state.scaleY", 0);
-        this.scale_container.draggable({disabled:true});
-        this.scale_image.hide();
-        this.scale_label.hide();
-        this.scale_button.attr('title','Show Scale Indicator');
-        this.scale_container.css({
-            'width'         : '10px',
-            'height'        : '10px',
-            'border'        : '1px solid #888',
-            'border-left'   : '0',
-            'border-bottom' : '0'
-        });
-        this.scale_container.css({
-            'position'      : 'absolute',
-            'top'           : 'auto',
-            'bottom'        : '0px',
-            'left'          : '0px'
-        });
-        this.scale_button.removeClass('minimize').addClass('maximize');
-        this.scale_button.removeClass('ui-icon-arrow-1-sw').addClass('ui-icon-arrow-1-ne');
-        this.scale_button.css({'margin':'-3px -3px 0 0'});
+        this.scale_button.attr('title','Show Earth-Scale Indicator');
+        this.scale_container.hide();
         this.scale_button.unbind();
         this.scale_button.bind('click',  $.proxy(this.earthMaximize,  this));
     },
 
     earthMaximize: function() {
+        var scaleXY;
+
+        scaleXY = this.resetIfOutsideViewportBounds();
+
         Helioviewer.userSettings.set("state.scale", true);
         Helioviewer.userSettings.set("state.scaleType", 'earth');
-        Helioviewer.userSettings.set("state.scaleX", 0);
-        Helioviewer.userSettings.set("state.scaleY", 0);
-        this.scale_container.draggable({disabled:false});
-        this.scale_image.show();
-        this.scale_label.show();
-        this.scale_button.attr('title','Hide Scale Indicator');
-        this.scale_container.css({
-            'position'      : 'absolute',
-            'top'           : 'auto',
-            'bottom'        : '0px',
-            'left'          : '0px',
-            'width'         : '73px',
-            'height'        : '56px',
-            'border'        : '1px solid #333',
-            'border-left'   : '0',
-            'border-bottom' : '0'
-        });
-        this.scale_button.removeClass('maximize').addClass('minimize');
-        this.scale_button.removeClass('ui-icon-arrow-1-ne').addClass('ui-icon-arrow-1-sw');
-        this.scale_button.css({'margin':'0'});
+        Helioviewer.userSettings.set("state.scaleX", scaleXY.x);
+        Helioviewer.userSettings.set("state.scaleY", scaleXY.y);
+        this.scale_button.attr('title','Hide Earth-Scale Indicator');
+        this.scale_container.show();
         this.scale_button.unbind();
         this.scale_button.bind('click',  $.proxy(this.earthMinimize,  this));
-    },
-
-    scaleContainerDrag: function() {
-        this.scale_container.css({
-            'border'        : '1px solid #888'
-        });
     },
 
     scaleContainerDragTo: function(containerX, containerY) {
         this.scale_container.css({
             'position' : 'absolute',
             'top'      : containerY+'px',
-            'left'     : containerX+'px',
-            'border'   : '1px solid #888'
+            'left'     : containerX+'px'
         });
+        this.scaleContainerDragStop();
     },
 
     scaleContainerDragStop: function(event) {
-        var coords;
+        var scaleXY;
+
+        scaleXY = this.resetIfOutsideViewportBounds();
+
+        Helioviewer.userSettings.set("state.scaleType",'earth');
+        Helioviewer.userSettings.set("state.scaleX",    scaleXY.x);
+        Helioviewer.userSettings.set("state.scaleY",    scaleXY.y);
+        Helioviewer.userSettings.set("state.containerX",this.scale_container.position().left);
+        Helioviewer.userSettings.set("state.containerY",this.scale_container.position().top);
+    },
+
+    resetIfOutsideViewportBounds: function(event) {
+        var scaleXY, coords;
 
         coords = new HelioviewerMouseCoordinates(Helioviewer.userSettings.get("state.imageScale"), 959.705, false);
-        coords = coords.computeMouseCoords(this.scale_container.offset().left, this.scale_container.offset().top)
 
-        // Snap back to docked position if dragged outside of Viewport bounds
-        if ( this.scale_container.position().left <= 0 ||
-             this.scale_container.position().left+this.scale_container.width() >= this.scale_container.parent().width()  ||
-             this.scale_container.position().top+this.scale_container.height() >= this.scale_container.parent().height() ||
-             this.scale_container.position().top  <= 0 ) {
+        // Snap back to default position if dragged outside of Viewport bounds
+        if ( Helioviewer.userSettings.get("state.containerX") <= 0 ||
+             Helioviewer.userSettings.get("state.containerX") >= this.scale_container.parent().width()-this.scale_container.width() ||
+             Helioviewer.userSettings.get("state.containerY") <= $('#hv-header').height() ||
+             Helioviewer.userSettings.get("state.containerY") >= this.scale_container.parent().height()-this.scale_container.height()
+            ) {
 
-            this.earthMaximize();
+            this.containerX = $('#helioviewer-viewport').width()/2 - this.scale_container.width()/2;
+            this.containerY = $('#helioviewer-viewport').height()/2 - this.scale_container.height()/2;
+
+            this.scale_container.css({
+                'position' : 'absolute',
+                'top'      : this.containerY+'px',
+                'left'     : this.containerX+'px'
+            });
         }
-        else {
-            Helioviewer.userSettings.set("state.scale",     true);
-            Helioviewer.userSettings.set("state.scaleType",'earth');
-            Helioviewer.userSettings.set("state.scaleX",    coords.x);
-            Helioviewer.userSettings.set("state.scaleY",    coords.y);
-            Helioviewer.userSettings.set("state.containerX",this.scale_container.position().left);
-            Helioviewer.userSettings.set("state.containerY",this.scale_container.position().top);
-        }
+
+        scaleXY = coords.computeMouseCoords(
+            Helioviewer.userSettings.get("state.containerX"),
+            Helioviewer.userSettings.get("state.containerY")
+        );
+
+        Helioviewer.userSettings.set("state.scaleType",'earth');
+        Helioviewer.userSettings.set("state.scaleX",    scaleXY.x);
+        Helioviewer.userSettings.set("state.scaleY",    scaleXY.y);
+
+        return scaleXY;
     },
 
 
@@ -204,21 +182,21 @@ var ImageScale = Class.extend(
     display: function() {
         this._getScaleSettings();
 
+        if ( Helioviewer.userSettings.get("state.scaleX") == 0 ||
+             Helioviewer.userSettings.get("state.scaleY") == 0 ) {
+
+            this.containerX = $('#helioviewer-viewport').width()/2 - this.scale_container.width()/2;
+            this.containerY = $('#helioviewer-viewport').height()/2 - this.scale_container.height()/2;
+            this.scale = false;
+        }
+
+        this.scaleContainerDragTo(this.containerX, this.containerY);
+
         if ( this.scale === false ) {
             this.earthMinimize();
         }
         else {
-
-            if ( this.scaleX == 0 || this.scaleY == 0 ) {
-                this.earthMaximize();
-            }
-            else {
-                this.scaleContainerDragTo(this.containerX, this.containerY);
-            }
+            this.earthMaximize();
         }
-
-        $(this.scale_container).css({
-            'display'  :'block'
-        });
     }
 });
