7ed991478462cd57b0ac71f551e453593bfc22cf
Jeff Stys <jstys@sesda3.com>
2015-03-03 11:00:23 -0500

    + Updated mouse coordinate buttons to toggle properly during all combinations of settings, including being toggled using the 'm' key.

--- src/js/Viewport/Helper/HelioviewerMouseCoordinates.js
+++ src/js/Viewport/Helper/HelioviewerMouseCoordinates.js
@@ -1,7 +1,7 @@
 /**
  * @author <a href="mailto:keith.hughitt@nasa.gov">Keith Hughitt</a>
  */
-/*jslint browser: true, white: true, onevar: true, undef: true, nomen: false, eqeqeq: true, plusplus: true, 
+/*jslint browser: true, white: true, onevar: true, undef: true, nomen: false, eqeqeq: true, plusplus: true,
 bitwise: true, regexp: true, strict: true, newcap: true, immed: true, maxlen: 120, sub: true */
 /*global $, Class, MouseCoordinates */
 "use strict";
@@ -14,35 +14,74 @@ var HelioviewerMouseCoordinates = MouseCoordinates.extend(
     init: function (imageScale, rsun, showMouseCoordsWarning) {
         this.rsun = rsun;
         this._super(imageScale, showMouseCoordsWarning);
+
+        this.buttonPolar     = $('#mouse-polar');
+        this.buttonCartesian = $('#mouse-cartesian');
+
+        this._initEventHandlers();
     },
-     
+
+    _initEventHandlers: function () {
+        $(document).bind('toggle-mouse-coords', $.proxy(this.toggleMouseCoords, this));
+        $(document).bind('polar-mouse-coords', $.proxy(this.polarMouseCoords, this));
+        $(document).bind('cartesian-mouse-coords', $.proxy(this.cartesianMouseCoords, this));
+    },
+
     /**
      * @description Toggles mouse-coords visibility
-     * 
+     *
      * TODO (2009/07/27) Disable mouse-coords display during drag & drop
      */
     toggleMouseCoords: function () {
-        // Case 1: Disabled -> Arcseconds 
+        // Case 1: Disabled -> Arcseconds
         if (this.mouseCoords === "disabled") {
-            this.mouseCoords = "arcseconds";
-            this.container.toggle();
+            this.cartesianMouseCoords();
         }
 
         // Case 2: Arcseconds -> Polar Coords
         else if (this.mouseCoords === "arcseconds") {
-            this.mouseCoords = "polar";
+            this.polarMouseCoords();
         }
 
         // Case 3: Polar Coords -> Disabled
-        else {
-            this.container.toggle();
+        else if (this.mouseCoords === "polar") {
+            this.container.hide();
             this.mouseCoords = "disabled";
+            this.buttonPolar.removeClass("active");
+            this.buttonCartesian.removeClass("active");
         }
-        
+
         this._checkWarning();
         this._reassignEventHandlers();
     },
-    
+
+    /**
+     * @description
+     */
+    cartesianMouseCoords: function () {
+
+        this.mouseCoords = "arcseconds";
+        this.container.show();
+        this.buttonCartesian.addClass("active");
+        this.buttonPolar.removeClass("active");
+
+        this._checkWarning();
+        this._reassignEventHandlers();
+    },
+
+    /**
+     * @description
+     */
+    polarMouseCoords: function () {
+        this.mouseCoords = "polar";
+        this.container.show();
+        this.buttonPolar.addClass("active");
+        this.buttonCartesian.removeClass("active");
+
+        this._checkWarning();
+        this._reassignEventHandlers();
+    },
+
     /**
      * Determines which event handler should be used, if any, to display mouse coordinates to the user
      */
@@ -58,28 +97,28 @@ var HelioviewerMouseCoordinates = MouseCoordinates.extend(
             if (this.mouseCoords === "polar") {
                 this.movingContainer.unbind('mousemove', this.updateMouseCoords);
             }
-               
-            this.movingContainer.bind('mousemove', $.proxy(this.updateMouseCoords, this));     
-               
+
+            this.movingContainer.bind('mousemove', $.proxy(this.updateMouseCoords, this));
+
             // Execute handler to display mouse coordinates placeholder
             this.movingContainer.trigger("mousemove");
         } else {
             this.movingContainer.unbind('mousemove', this.updateMouseCoords);
         }
     },
-    
+
     /**
      * updateMouseCoords
      */
     updateMouseCoords: function (event) {
         var cartesian, polar, r, theta;
-        
+
         if (!this.enabled) {
             return;
         }
 
         // When mouse-coordinates are first turned on just shows dashes
-        if (typeof(event.pageX) == "undefined") {
+        if ( typeof(event.pageX) == "undefined") {
             if (this.mouseCoords === "arcseconds") {
                 this.showCartesianCoordinates("--", "--");
             } else {
@@ -87,10 +126,10 @@ var HelioviewerMouseCoordinates = MouseCoordinates.extend(
             }
             return;
         }
-            
+
         // Compute coordinates relative to top-left corner of the viewport
         cartesian = this.computeMouseCoords(event.pageX, event.pageY);
-                            
+
         // Arc-seconds
         if (this.mouseCoords === "arcseconds") {
             this.showCartesianCoordinates(cartesian.x, cartesian.y);
@@ -99,11 +138,11 @@ var HelioviewerMouseCoordinates = MouseCoordinates.extend(
             polar = Math.toPolarCoords(cartesian.x, cartesian.y);
             r     = ((polar.r / this.rsun) + "").substring(0, 5);
             theta = Math.round(polar.theta);
-            
-            this.showPolarCoordinates(r, theta); 
+
+            this.showPolarCoordinates(r, theta);
         }
     },
-    
+
     /**
      * Displays cartesian coordinates in arc-seconds
      */
@@ -111,7 +150,7 @@ var HelioviewerMouseCoordinates = MouseCoordinates.extend(
         this.mouseCoordsX.html("x: " + x + " &prime;&prime;");
         this.mouseCoordsY.html("y: " + y + " &prime;&prime;");
     },
-    
+
     /**
      * Displays cartesian coordinates in arc-seconds
      */
