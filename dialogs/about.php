cd9b7026d69a13b9013a43cfac86ae3d22bcb958
Serge Zahniy <ddialer@gmail.com>
2017-03-10 12:30:21 -0500

    Add Helioviewer.org version number to "About" dialog #190;

--- dialogs/about.php
+++ dialogs/about.php
@@ -1,13 +1,13 @@
 
 <img src="resources/images/logos/about_white.png" alt="Helioviewer.org Logo"><br>
-<!--<div style="width:100%; text-align: center;">
-    <span style="margin-left: auto; margin-right: auto; font-size:small;">Last Updated: 2015/09/29 (2.4.0)</span>
-</div>-->
+<div style="width:100%; text-align: center;">
+    <span style="margin-left: auto; margin-right: auto; font-size:small;">Last Updated: 2017/03/10 (v3.2.0)</span>
+</div>
 <br />
 
-Helioviewer.org is part of the <a href="https://github.com/Helioviewer-Project" style="text-decoration: underline; font-weight: normal;" class="light" target="_blank">Helioviewer Project</a>, an open-source project for the visualization of solar and heliospheric data.
+Helioviewer.org is part of the <a href="https://github.com/Helioviewer-Project" style="font-weight: normal;" class="light" target="_blank">Helioviewer Project</a>, an open-source project for the visualization of solar and heliospheric data.
 The Helioviewer Project is funded by <acronym title="European Space Agency">ESA</acronym> and <acronym title="National Aeronautics and Space Administration">NASA</acronym>.
-For more information, please visit our <a href="http://helioviewer.org/wiki/Main_Page" style="text-decoration: underline; font-weight: normal;" class="light" target="_blank">Wiki</a>.
+For more information, please visit our <a href="http://helioviewer.org/wiki/Main_Page" style="font-weight: normal;" class="light" target="_blank">Wiki</a>.
 
 <br /><br />
 
@@ -20,11 +20,13 @@ For more information, please visit our <a href="http://helioviewer.org/wiki/Main
         <a href="http://www.nasa.gov/mission_pages/stereo/main/index.html" target="_blank">STEREO (NASA)</a>,
         <a href="http://sci.esa.int/proba2/" target="_blank">PROBA2 (ESA)</a>,
         <a href="http://www.isas.ac.jp/e/enterp/missions/yohkoh/index.shtml" target="_blank">Yohkoh (JAXA/NASA/PPARC)</a>,
+        <a href="http://www.isas.jaxa.jp/home/solar/" target="_blank">Hinode (JAXA/NASA/PPARC)</a>,
         <a href="http://www.nasa.gov/centers/goddard/home/index.html" title="Goddard Space Flight Center" target="_blank">GSFC</a>,
         <a href="http://www.astro.oma.be/" title="Royal Observatory of Belgium" target="_blank">Royal Observatory of Belgium</a>,
         <a href="http://www.lmsal.com" title="Lockheed Martin Solar &amp; Astrophysics Laboratory" target="_blank">LMSAL</a>,
         <a href="http://umbra.nascom.nasa.gov/" title="Solar Data Analysis Center" target="_blank">SDAC</a>,
         <a href="http://sun.stanford.edu/" target="_blank">Stanford University</a>,
+        <a href="https://www.cfa.harvard.edu/" target="_blank">Harvard-Smithsonian Astrophysical Observatory</a>,
         <a href="http://solar.physics.montana.edu/sol_phys/fft/" target="_blank">MSU/SDO-FFT</a>
     </span>
 </div>
@@ -33,8 +35,8 @@ For more information, please visit our <a href="http://helioviewer.org/wiki/Main
 <div id="about-credits" style="width:100%; text-align:center;">
     <span style="margin-left:auto; margin-right: auto;">
         <b>Helioviewer.org makes use of the following tools:</b><br />
-        <a href="https://www.browserstack.com/" style="text-decoration: underline;" target="_blank">BrowserStack</a> - Cross Browser Testing Tool.<br/>
-        <a href="http://www.highcharts.com/" style="text-decoration: underline;" target="_blank">Highcharts</a> - Interactive JavaScript charts for your webpage.<br/>
+        <a href="https://www.browserstack.com/" target="_blank">BrowserStack</a> - Cross Browser Testing Tool.<br/>
+        <a href="http://www.highcharts.com/" target="_blank">Highcharts</a> - Interactive JavaScript charts for your webpage.<br/>
 
     </span>
 </div>
