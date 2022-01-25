bf35eacd6a381ce09c536a039fd1a9dedb97a066
btaylor-sesda <87322123+btaylor-sesda@users.noreply.github.com>
2022-01-25 08:20:44 -0500

    Update Tutorial.js

--- resources/js/Utility/Tutorial.js
+++ resources/js/Utility/Tutorial.js
@@ -21,9 +21,9 @@ function startTutorial(){
 			text: '<b>Features & Events</b><br><br>Use the Solar Features & Events checkbox interface to select annotations to overlay onto the images in the viewport. <a href="http://wiki.helioviewer.org/wiki/Helioviewer.org_User_Guide_3.1.0#Events_selector" target="_blank">More... <span class="fa fa-external-link fa-fw"></span></a>',
 			position: 'top_right'
 		},{
-			selector: '#accordion-events',
-			text: '<b>Celestial Bodies</b><br><br>Use the Celestial Bodies checkbox interface to select Labels and/or Trajectories to overlay onto the images in the viewport. Use the Next/Last buttons in the Trajectory sub-section to skip to the Next/Last trajectory relative to the current observation time. Click on Celestial Body and Satelite labels to reveal more information. Hover over points in trajectory overlay to reveal date information. Click on points in trajectory overlay to set the observation time to that point.',
-			position: 'top_right'
+			selector: '#accordion-bodies',
+			text: '<script>if($("#accordion-events .disclosure-triangle:first").hasClass("closed")===false){$("#accordion-events .disclosure-triangle:first").addClass("closed");}</script><b>Celestial Bodies</b><br><br>Use the Celestial Bodies checkbox interface to select Labels and/or Trajectories to overlay onto the images in the viewport. Use the Next/Last buttons in the Trajectory sub-section to skip to the Next/Last trajectory relative to the current observation time. Click on Celestial Body and Satelite labels to reveal more information. Hover over points in trajectory overlay to reveal date information. Click on points in trajectory overlay to set the observation time to that point.',
+			position: 'best'
 		},{
 			selector: '#zoom',
 			text: '<b>Zoom tool</b><br><br>Allows zooming in/out and recentering of data in the viewer window. <a href="http://wiki.helioviewer.org/wiki/Helioviewer.org_User_Guide_3.1.0#Zooming_tool" target="_blank">More... <span class="fa fa-external-link fa-fw"></span></a>',
