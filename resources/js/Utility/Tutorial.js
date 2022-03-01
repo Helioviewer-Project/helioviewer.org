84cc13f42afdf17340c3418825a5d5e1c4591f58
btaylor-sesda <87322123+btaylor-sesda@users.noreply.github.com>
2022-03-01 13:49:03 -0500

    Update Tutorial.js

--- resources/js/Utility/Tutorial.js
+++ resources/js/Utility/Tutorial.js
@@ -38,14 +38,14 @@ function startTutorial(){
 			position: 'bottom'
 		},{
 			selector: '#menus',
-			text: '<b>Navigation Menu</b><br><br><p style="text-align:left"><img src="http://wiki.helioviewer.org/w/images/c/c7/Icon-rss.png"> : Helioviewer Project Announcements.<BR>\
-<img src="http://wiki.helioviewer.org/w/images/9/9f/Icon-youtube.png"> : View Helioviewer Movies Shared to YouTube.<BR>\
-<img src="http://wiki.helioviewer.org/w/images/5/53/Icon-movie.png"> : Create a movie.<BR>\
-<img src="http://wiki.helioviewer.org/w/images/6/69/Icon-screenshot.png"> : Create a screenshot.<BR>\
-<img src="http://wiki.helioviewer.org/w/images/6/61/Icon-data.png"> : Request Science Data Download from External Partners.<BR>\
-<img src="http://wiki.helioviewer.org/w/images/3/33/Icon-share.png"> : Share the current viewport on social media.<BR>\
-<img src="http://wiki.helioviewer.org/w/images/8/87/Icon-help.png"> : Get Help with Helioviewer.<BR>\
-<img src="http://wiki.helioviewer.org/w/images/e/e1/Icon-settings.png"> : Edit Settings &amp; Defaults.</p><br></br> <a href="http://wiki.helioviewer.org/wiki/Helioviewer.org_User_Guide_3.1.0#Sidebar_Panels" target="_blank">Click here <span class="fa fa-external-link fa-fw"></span></a> to read about each menu item and functionality.',
+			text: '<b>Navigation Menu</b><br><br><p style="text-align:left"><img src="https://beta.helioviewer.org/resources/images/projectanounce_icon_white.png"> : Helioviewer Project Announcements.<BR>\
+<img src="https://beta.helioviewer.org/resources/images/viewyoutube_icon_white.png"> : View Helioviewer Movies Shared to YouTube.<BR>\
+<img src="https://beta.helioviewer.org/resources/images/createmovie_icontest.png"> : Create a movie.<BR>\
+<img src="https://beta.helioviewer.org/resources/images/screenshot_icon_white.png"> : Create a screenshot.<BR>\
+<img src="https://beta.helioviewer.org/resources/images/datadownload_icon_white.png"> : Request Science Data Download from External Partners.<BR>\
+<img src="https://beta.helioviewer.org/resources/images/shareviewport_icon_white.png"> : Share the current viewport on social media.<BR>\
+<img src="https://beta.helioviewer.org/resources/images/help_icon_white.png"> : Get Help with Helioviewer.<BR>\
+<img src="https://beta.helioviewer.org/resources/images/setting_icon_white.png"> : Edit Settings &amp; Defaults.</p><br></br> <a href="http://wiki.helioviewer.org/wiki/Helioviewer.org_User_Guide_3.1.0#Sidebar_Panels" target="_blank">Click here <span class="fa fa-external-link fa-fw"></span></a> to read about each menu item and functionality.',
 			position: 'bottom'
 		},{
 			selector: '#hv-drawer-tab-timeline',
