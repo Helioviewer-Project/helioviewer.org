ad7ff00d10e6d4250f41297e64cfecbfc8aff9a8
btaylor-sesda <87322123+btaylor-sesda@users.noreply.github.com>
2022-04-13 12:57:58 -0400

    Create responsive_hv.js

---
+++ resources/js/responsive/responsive_hv.js
@@ -0,0 +1,48 @@
+// zeynepjs initialization for demo
+$(function() {
+
+  var zeynep = $('.zeynep').zeynep({
+    opened: function () {
+      console.log('the side menu is opened')
+    }
+  })
+
+  // dynamically bind 'closing' event
+  zeynep.on('closing', function () {
+    console.log('this event is dynamically binded')
+  })
+
+  // handle zeynepjs overlay click
+  $('.zeynep-overlay').on('click', function () {
+    $(".hamburger").removeClass("is-active");
+	zeynep.close();
+  })
+
+  // open zeynepjs side menu
+  $('.btn-open, .hamburger').on('click', function () {
+    if($(".hamburger").hasClass("is-active")) {
+		$(".hamburger").removeClass("is-active");
+		zeynep.close();
+	}
+	else {
+		zeynep.open();
+		$(".hamburger").addClass("is-active");
+	}
+  })
+
+
+	// open drawer by tab click
+	var currdsopen= 'nonexistentds';
+	$(".hvmobdstabs").click(function(){
+		$('.hvmodbstabs').css('background-image','url(https://develop.helioviewer.org/resources/images/backmobilemenubg2.png)');
+		var thisdrawersect= this.attr('drawersec');
+		// if it's not already open, close currently open drawer and open correct one
+		if(thisdrawersect != currdsopen) {
+			$('#'+currdsopen).slideDown("slow");
+			$('#'+thisdrawersect).slideUp("slow");
+			currdsopen= thisdrawersect;
+		}
+
+	});
+
+});
