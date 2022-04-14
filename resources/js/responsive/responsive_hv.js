cac595e4a6818534c4c575118e6209e33bbbad94
btaylor-sesda <87322123+btaylor-sesda@users.noreply.github.com>
2022-04-14 14:18:36 -0400

    Update responsive_hv.js

--- resources/js/responsive/responsive_hv.js
+++ resources/js/responsive/responsive_hv.js
@@ -34,13 +34,14 @@ $(function() {
 	// open drawer by tab click
 	var currdsopen= 'nonexistentds';
 	$(".hvmobdstabs").click(function(){
-		$('.hvmodbstabs').css('background-image','url(https://develop.helioviewer.org/resources/images/backmobilemenubg2.png)');
+		$('.hvmodbstabs').css({'background-image':'url(https://develop.helioviewer.org/resources/images/backmobilemenubg2.png)'});
 		var hvmobtabid = $(this).attr('drawersec')+'_mobtab';
 		console.log(hvmobtabid);
 		$('#'+hvmobtabid).css({'background-image':'url(https://develop.helioviewer.org/resources/images/mobiletabbgwhite1.png)','color':'black'});
 		var thisdrawersect= $(this).attr('drawersec');
 		// if it's not already open, close currently open drawer and open correct one
 		if(thisdrawersect != currdsopen) {
+			$('#hv-drawer-left').attr('style', '');
 			$('#hv-drawer-left').fadeOut("fast");
 			$('#'+currdsopen).css('display','none');
 			$('#'+thisdrawersect).css('display','block');
