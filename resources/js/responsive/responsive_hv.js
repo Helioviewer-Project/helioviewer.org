abee05979862cd81680117bb39f1ed2b589de6c9
btaylor-sesda <87322123+btaylor-sesda@users.noreply.github.com>
2022-04-14 09:13:33 -0400

    Update responsive_hv.js

--- resources/js/responsive/responsive_hv.js
+++ resources/js/responsive/responsive_hv.js
@@ -36,6 +36,7 @@ $(function() {
 	$(".hvmobdstabs").click(function(){
 		$('.hvmodbstabs').css('background-image','url(https://develop.helioviewer.org/resources/images/backmobilemenubg2.png)');
 		var hvmobtabid = $(this).attr('id');
+		console.log(hvmobtabid);
 		$('#'+hvmobtabid).css({'background-image':'url(https://develop.helioviewer.org/resources/images/mobiletabbgwhite1.png)','color':'black'});
 		var thisdrawersect= $('#'+hvmobtabid).attr('drawersec'); //$(this)
 		// if it's not already open, close currently open drawer and open correct one
