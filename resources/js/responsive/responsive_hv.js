5c542adce521d0c1a05283917c521e89bca6336f
btaylor-sesda <87322123+btaylor-sesda@users.noreply.github.com>
2022-04-15 11:27:21 -0400

    Update responsive_hv.js

--- resources/js/responsive/responsive_hv.js
+++ resources/js/responsive/responsive_hv.js
@@ -52,3 +52,21 @@ $(function() {
 	});
 
 });
+
+// Create a condition that targets viewports at least 768px wide
+const mediaQuery = window.matchMedia('(max-width: 790px)');
+
+function handleTabletChange(e) {
+  // Check if the media query is true
+  if (e.matches) {
+    // Then log the following message to the console
+    console.log('Media Query Matched!');
+	$('#hv-drawer-left').attr('style', 'display: none');
+  }
+}
+
+// Register event listener
+mediaQuery.addListener(handleTabletChange);
+
+// Initial check
+handleTabletChange(mediaQuery);
