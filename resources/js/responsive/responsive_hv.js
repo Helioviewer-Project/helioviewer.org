a0af20aede51847cbf300ff02477a62b8b91569d
btaylor-sesda <87322123+btaylor-sesda@users.noreply.github.com>
2022-04-15 14:56:14 -0400

    Update responsive_hv.js

--- resources/js/responsive/responsive_hv.js
+++ resources/js/responsive/responsive_hv.js
@@ -1,15 +1,16 @@
 // zeynepjs initialization for demo
 $(function() {
 
+
   var zeynep = $('.zeynep').zeynep({
     opened: function () {
-      console.log('the side menu is opened')
+      //console.log('the side menu is opened')
     }
   })
 
   // dynamically bind 'closing' event
   zeynep.on('closing', function () {
-    console.log('this event is dynamically binded')
+    //console.log('this event is dynamically binded')
   })
 
   // handle zeynepjs overlay click
@@ -36,7 +37,7 @@ $(function() {
 	$(".hvmobdstabs").click(function(){
 		$('.hvmodbstabs').css({'background-image':'url(https://develop.helioviewer.org/resources/images/backmobilemenubg2.png)'});
 		var hvmobtabid = $(this).attr('drawersec')+'_mobtab';
-		console.log(hvmobtabid);
+		// console.log(hvmobtabid);
 		$('#'+hvmobtabid).css({'background-image':'url(https://develop.helioviewer.org/resources/images/mobiletabbgwhite1.png)','color':'black'});
 		var thisdrawersect= $(this).attr('drawersec');
 		// if it's not already open, close currently open drawer and open correct one
@@ -53,7 +54,9 @@ $(function() {
 
 });
 
-// Create a condition that targets viewports at least 768px wide
+// START media query 
+
+// Create a condition that targets viewports at least 790px wide
 const mediaQuery = window.matchMedia('(max-width: 790px)');
 
 function handleTabletChange(e) {
@@ -71,3 +74,6 @@ mediaQuery.addListener(handleTabletChange);
 
 // Initial check
 handleTabletChange(mediaQuery);
+
+
+// END media query 
