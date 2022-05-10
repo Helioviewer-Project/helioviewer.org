29326d5bea894137d75c504e7b994837e69c2bc9
btaylor-sesda <87322123+btaylor-sesda@users.noreply.github.com>
2022-05-10 10:15:05 -0400

    Update CustomHandling.js

--- resources/js/CustomHandling/CustomHandling.js
+++ resources/js/CustomHandling/CustomHandling.js
@@ -228,7 +228,8 @@ $(document).ready(function() {
 
 // if SOHO or Stereo-A PSP are checked, then run the PSP Encounter code updates
 function timedPSPEncounterCall() {
-	if($('#soho-psp-tree-branch').hasClass('jstree-checked') || $('#stereo_a-psp-tree-branch').hasClass('jstree-checked')) {
+	if($('#soho-psp-tree-branch').hasClass('jstree-checked') || $('#soho-psp-tree-branch').hasClass('jstree-checked')) {
+		//setTimeout(function(){$('#soho-psp-tree-trajectory .button:first').trigger('click');},800);
 		$('#soho-psp-tree-trajectory .button:first').trigger('click');
 	}
 }
