b540069e1398a31c039484128fed896818898c84
btaylor-sesda <87322123+btaylor-sesda@users.noreply.github.com>
2022-06-06 07:31:37 -0400

    Update responsive_hv.js

--- resources/js/responsive/responsive_hv.js
+++ resources/js/responsive/responsive_hv.js
@@ -96,7 +96,6 @@ $(function() {
 			$('#hv-drawer-left').attr('style', 'display: none');
 			$('.hvmobdstabs .hvmobds_icon').css('filter','invert(81%) sepia(7%) saturate(4%) hue-rotate(6deg) brightness(95%) contrast(91%)');
 			$('.hvmobdstabs span').css({'color':'silver'});
-			currdsopen= 'nonexistentds';
 		}		
 		
 		// if it's not already open, close currently open drawer and open correct one
