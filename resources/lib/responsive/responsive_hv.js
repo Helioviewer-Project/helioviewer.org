a8cc4c3fe9cfec51de45c2d02c02c035e9676fc3
btaylor-sesda <87322123+btaylor-sesda@users.noreply.github.com>
2022-07-06 14:24:55 -0400

    Update responsive_hv.js

--- resources/lib/responsive/responsive_hv.js
+++ resources/lib/responsive/responsive_hv.js
@@ -366,7 +366,59 @@ function hvOnResize() {
 	setTimeout(function(){datetimemobModule();},2000);
 	
 	
-	
+/*  ------------  START interactJS move & pinch-to-zoom --------------- */
+// from https://interactjs.io/
+
+
+	var angleScale = {
+  angle: 0,
+  scale: 1
+}
+var gestureArea = document.getElementById('moving-contatiner')
+var scaleElement = document.getElementsByClassName('tile-layer-container')[0]
+var resetTimeout
+
+interact(gestureArea)
+  .gesturable({
+    listeners: {
+      start (event) {
+        angleScale.angle -= event.angle
+
+        clearTimeout(resetTimeout)
+        scaleElement.classList.remove('reset')
+      },
+      move (event) {
+        // document.body.appendChild(new Text(event.scale))
+        var currentAngle = event.angle + angleScale.angle
+        var currentScale = event.scale * angleScale.scale
+
+        scaleElement.style.transform =
+          'rotate(' + currentAngle + 'deg)' + 'scale(' + currentScale + ')'
+
+        // uses the dragMoveListener from the draggable demo above
+        dragMoveListener(event)
+      },
+      end (event) {
+        angleScale.angle = angleScale.angle + event.angle
+        angleScale.scale = angleScale.scale * event.scale
+
+        resetTimeout = setTimeout(reset, 1000)
+        scaleElement.classList.add('reset')
+      }
+    }
+  })
+  .draggable({
+    listeners: { move: dragMoveListener }
+  })
+
+function reset () {
+  scaleElement.style.transform = 'scale(1)'
+
+  angleScale.angle = 0
+  angleScale.scale = 1
+}
+
+/*  ------------  END interactJS move & pinch-to-zoom --------------- */
 	
 
 
