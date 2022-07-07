56335facdf8d7132af54125439f473649be349dd
btaylor-sesda <87322123+btaylor-sesda@users.noreply.github.com>
2022-07-07 13:11:14 -0400

    Update responsive_hv.js

--- resources/lib/responsive/responsive_hv.js
+++ resources/lib/responsive/responsive_hv.js
@@ -365,60 +365,141 @@ function hvOnResize() {
 	
 	setTimeout(function(){datetimemobModule();},2000);
 	
-	
-/*  ------------  START interactJS move & pinch-to-zoom --------------- */
-// from https://interactjs.io/
 
 
-	var angleScale = {
-  angle: 0,
-  scale: 1
-}
-var gestureArea = document.getElementById('moving-contatiner')
-var scaleElement = document.getElementsByClassName('tile-layer-container')[0]
-var resetTimeout
 
-interact(gestureArea)
-  .gesturable({
-    listeners: {
-      start (event) {
-        angleScale.angle -= event.angle
 
-        clearTimeout(resetTimeout)
-        scaleElement.classList.remove('reset')
-      },
-      move (event) {
-        // document.body.appendChild(new Text(event.scale))
-        var currentAngle = event.angle + angleScale.angle
-        var currentScale = event.scale * angleScale.scale
 
-        scaleElement.style.transform =
-          'rotate(' + currentAngle + 'deg)' + 'scale(' + currentScale + ')'
 
-        // uses the dragMoveListener from the draggable demo above
-        dragMoveListener(event)
-      },
-      end (event) {
-        angleScale.angle = angleScale.angle + event.angle
-        angleScale.scale = angleScale.scale * event.scale
 
-        resetTimeout = setTimeout(reset, 1000)
-        scaleElement.classList.add('reset')
-      }
-    }
-  })
-  .draggable({
-    listeners: { move: dragMoveListener }
-  })
 
-function reset () {
-  scaleElement.style.transform = 'scale(1)'
 
-  angleScale.angle = 0
-  angleScale.scale = 1
+
+
+
+
+
+
+/* -------------- START pinch-to-zoom functionality -------------- */
+
+// Log events flag
+var hvmobpnchlogEv = false;
+
+// Global vars to cache event state
+var hvmobpncevCache = new Array();
+var hvmobpncprevDiff = -1;
+
+// Logging/debugging functions
+function enableLog(hvmobev) {
+  hvmobpnchlogEv = hvmobpnchlogEv ? false : true;
 }
 
-/*  ------------  END interactJS move & pinch-to-zoom --------------- */
-	
+function log(prefix, hvmobev) {
+  if (!hvmobpnchlogEv) return;
+  var hvmobpnchlogoutput = document.getElementsByTagName('output')[0];
+  var hvmob_s = prefix + ": pointerID = " + hvmobev.pointerId +
+                " ; pointerType = " + hvmobev.pointerType +
+                " ; isPrimary = " + hvmobev.isPrimary;
+  hvmobpnchlogoutput.innerHTML += hvmob_s + " <br>";
+} 
 
+function clearLog(event) {
+ var hvmobpnchlogoutput = document.getElementsByTagName('output')[0];
+ hvmobpnchlogoutput.innerHTML = "";
+}
 
+function pointerdown_handler(hvmobev) {
+ // The pointerdown event signals the start of a touch interaction.
+ // This event is cached to support 2-finger gestures
+ hvmobpncevCache.push(hvmobev);
+ log("pointerDown", hvmobev);
+}
+
+function pointermove_handler(hvmobev) {
+ // This function implements a 2-pointer horizontal pinch/zoom gesture. 
+ //
+ // If the distance between the two pointers has increased (zoom in), 
+ // the taget element's background is changed to "pink" and if the 
+ // distance is decreasing (zoom out), the color is changed to "lightblue".
+ //
+ // This function sets the target element's border to "dashed" to visually
+ // indicate the pointer's target received a move event.
+ log("pointerMove", hvmobev);
+ hvmobev.target.style.border = "dashed";
+
+ // Find this event in the cache and update its record with this event
+ for (var hvmobevitem = 0; hvmobevitem < hvmobpncevCache.length; hvmobevitem++) {
+   if (hvmobev.pointerId == hvmobpncevCache[hvmobevitem].pointerId) {
+      hvmobpncevCache[hvmobevitem] = hvmobev;
+   break;
+   }
+ }
+
+ // If two pointers are down, check for pinch gestures
+ if (hvmobpncevCache.length == 2) {
+   // Calculate the distance between the two pointers
+   var curDiff = Math.abs(hvmobpncevCache[0].clientX - hvmobpncevCache[1].clientX);
+
+   if (hvmobpncprevDiff > 0) {
+     if (curDiff > hvmobpncprevDiff) {
+       // The distance between the two pointers has increased
+       log("Pinch moving OUT -> Zoom in", hvmobev);
+       //hvmobev.target.style.background = "pink";
+	   
+	   $('#zoom-in-button').trigger('click');
+	   
+     }
+     if (curDiff < hvmobpncprevDiff) {
+       // The distance between the two pointers has decreased
+       log("Pinch moving IN -> Zoom out",hvmobev);
+       //hvmobev.target.style.background = "lightblue";
+	   
+	   $('#zoom-out-button').trigger('click');
+	   
+     }
+   }
+
+   // Cache the distance for the next move event 
+   hvmobpncprevDiff = curDiff;
+ }
+}
+
+function pointerup_handler(hvmobev) {
+  log(hvmobev.type, hvmobev);
+  // Remove this pointer from the cache and reset the target's
+  // background and border
+  remove_event(hvmobev);
+  hvmobev.target.style.background = "white";
+  hvmobev.target.style.border = "1px solid black";
+ 
+  // If the number of pointers down is less than two then reset diff tracker
+  if (hvmobpncevCache.length < 2) hvmobpncprevDiff = -1;
+}
+
+function remove_event(hvmobev) {
+ // Remove this event from the target's cache
+ for (var hvmobevitem = 0; hvmobevitem < hvmobpncevCache.length; hvmobevitem++) {
+   if (hvmobpncevCache[hvmobevitem].pointerId == hvmobev.pointerId) {
+     hvmobpncevCache.splice(hvmobevitem, 1);
+     break;
+   }
+ }
+}
+
+function hvmobpnchinit() {
+	// Install event handlers for the pointer target
+	var hvmobel=document.getElementById("moving-container");
+	hvmobel.onpointerdown = pointerdown_handler;
+	hvmobel.onpointermove = pointermove_handler;
+
+	// Use same handler for pointer{up,cancel,out,leave} events since
+	// the semantics for these events - in this app - are the same.
+	hvmobel.onpointerup = pointerup_handler;
+	hvmobel.onpointercancel = pointerup_handler;
+	hvmobel.onpointerout = pointerup_handler;
+	hvmobel.onpointerleave = pointerup_handler;
+}
+
+hvmobpnchinit();
+ 
+ /* ------------- END pinch-to-zoom functionality -------------- */
