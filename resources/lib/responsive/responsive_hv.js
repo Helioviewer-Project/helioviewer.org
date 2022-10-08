3d12794a0925b0210cabd075c3718e865e36500c
btaylor-sesda <87322123+btaylor-sesda@users.noreply.github.com>
2022-10-08 13:41:27 -0400

    Update responsive_hv.js

--- resources/lib/responsive/responsive_hv.js
+++ resources/lib/responsive/responsive_hv.js
@@ -328,9 +328,10 @@ $(function() {
 	});
 	
 	// close mobile event popup when close button pressed or body is touched/clicked
+	/*
 	$('.event-popup .close-button').click(function(){
 		$('#event-popup_mob').css('display','none');
-	});
+	}); */
 
 	// on orientation change [portait / landscape]
 	$(window).on('orientationchange resize', function () {
