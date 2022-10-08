e4aa0c4a8c15b023fa71faf9d1ca15ee409f0c2d
btaylor-sesda <87322123+btaylor-sesda@users.noreply.github.com>
2022-10-08 13:55:22 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1779,7 +1779,7 @@ echo $hvdesktopcsshides;
 		<div id="helioviewer-viewport-container-inner">
 			<div id="helioviewer-viewport">
                 <!-- START mobile touchscreen viewport div -->	
-                <div id="toptouchlayer"></div>
+                <!-- <div id="toptouchlayer"></div> -->
                  <!-- END mobile touchscreen viewport div -->	
 
 				<!-- Movement sandbox -->
