bcdfa44915ea957bcc5aa02b56168c5097f91618
btaylor-sesda <87322123+btaylor-sesda@users.noreply.github.com>
2022-10-15 17:44:43 -0400

    Update responsive_hv.js

--- resources/lib/responsive/responsive_hv.js
+++ resources/lib/responsive/responsive_hv.js
@@ -387,7 +387,7 @@ $(document.body).on('click','.event-marker', function(){
 			setTimeout(function () {
 				console.log('event marker clicked');
 				//evpopuphtml= $(this).html();
-				evpopuphtml= obj.innerHTML;
+				evpopuphtml= obj.text;
 				observer.observe(obj);
 				//observer.observe($(this).html());
 			}, 400);
