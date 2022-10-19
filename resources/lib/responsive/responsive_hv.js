da6f2a510bd4c926c732adb500d481bb8f7fd852
btaylor-sesda <87322123+btaylor-sesda@users.noreply.github.com>
2022-10-19 13:20:12 -0400

    Update responsive_hv.js

--- resources/lib/responsive/responsive_hv.js
+++ resources/lib/responsive/responsive_hv.js
@@ -441,18 +441,18 @@ $(document.body).on('click','#event-popup_mob .event-create-movie', function(){
 });
 
 // trigger event popup click to search (1) 
-$(document.body).on('click','#event-popup_mob .event-search-external:nth-child(1)', function(){
+$(document.body).on('click','#event-popup_mob > div > .container > div.btn-container > div.event-search-external:nth-child(1)', function(){
 	console.log('1st search clicked'+' '+$(this).attr('data-url'));
 	let evIDtoaccess= $('#event-popup_mob').attr('rel');
-	$('#'+evIDtoaccess+' .event-search-external:nth-child(1)').trigger("click");
+	$('#'+evIDtoaccess+' div.event-search-external:nth-child(1)').trigger("click");
 	closeMobEventPopup();
 });
 
 // trigger event popup click to search (2) 
-$(document.body).on('click','#event-popup_mob .event-search-external:nth-child(2)', function(){
+$(document.body).on('click','#event-popup_mob > div > .container > div.btn-container > div.event-search-external:nth-child(2)', function(){
 	console.log('2nd search clicked'+' '+$(this).attr('data-url'));
 	let evIDtoaccess= $('#event-popup_mob').attr('rel');
-	$('#'+evIDtoaccess+' .event-search-external:nth-child(2)').trigger("click");
+	$('#'+evIDtoaccess+' div.event-search-external:nth-child(2)').trigger("click");
 	closeMobEventPopup();
 });
 
