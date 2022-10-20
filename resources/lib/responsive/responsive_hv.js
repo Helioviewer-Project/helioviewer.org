923dba4151409faedb0c1c1a7ef021eab83c51d8
btaylor-sesda <87322123+btaylor-sesda@users.noreply.github.com>
2022-10-20 12:41:47 -0400

    Update responsive_hv.js

--- resources/lib/responsive/responsive_hv.js
+++ resources/lib/responsive/responsive_hv.js
@@ -465,7 +465,7 @@ let thiscbtype='';
 // when celestial bodies are clicked
 $(document.body).on('mousedown','.celestial-bodies-label', function(){
 	console.log('cb clicked');
-	/*
+	
 	closeMobEventPopup();
 	
 	// this celestial body label ID
@@ -498,7 +498,7 @@ $(document.body).on('mousedown','.celestial-bodies-label', function(){
 
 	cbpopuphtml= $('#'+cbgetpopupid).html();
 	$('#event-popup_mob').html(cbpopuphtml);
-	*/
+	
 });
 
 
