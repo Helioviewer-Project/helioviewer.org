454413c936263084830b72d8036830506c29aa37
btaylor-sesda <87322123+btaylor-sesda@users.noreply.github.com>
2022-10-21 18:11:00 -0400

    Update responsive_hv.js

--- resources/lib/responsive/responsive_hv.js
+++ resources/lib/responsive/responsive_hv.js
@@ -2,24 +2,33 @@
 $(function() {
 
 	// observer for selecting dynamic elements
-	function waitForElm(selector) {
-		return new Promise(resolve => {
-			if (document.querySelector(selector)) {
-				return resolve(document.querySelector(selector));
-			}
+	export function _waitForElement(selector, delay = 10, tries = 100) {
+	  const element = document.querySelector(selector);
 
-			const observer = new MutationObserver(mutations => {
-				if (document.querySelector(selector)) {
-					resolve(document.querySelector(selector));
-					observer.disconnect();
-				}
-			});
 
-			observer.observe(document.body, {
-				childList: true,
-				subtree: true
-			});
+	  if (!window[`__${selector}`]) {
+		window[`__${selector}`] = 0;
+		window[`__${selector}__delay`] = delay;
+		window[`__${selector}__tries`] = tries;
+	  }
+
+	  function _search() {
+		return new Promise((resolve) => {
+		  window[`__${selector}`]++;
+		  setTimeout(resolve, window[`__${selector}__delay`]);
 		});
+	  }
+
+	  if (element === null) {
+		if (window[`__${selector}`] >= window[`__${selector}__tries`]) {
+		  window[`__${selector}`] = 0;
+		  return Promise.resolve(null);
+		}
+
+		return _search().then(() => _waitForElement(selector));
+	  } else {
+		return Promise.resolve(element);
+	  }
 	}
 
 	// positioning movie player
@@ -571,6 +580,9 @@ $(document.body).on('click','.celestial-bodies-label', function(){
 		cbpopuphtml= $('#'+cbgetpopupid).html();
 		$('#event-popup_mob').html(cbpopuphtml);
 	});
+	
+	cbpopuphtml = await waitForElement('#'+cbgetpopupid);
+    console.log(cbpopuphtml);
 	
 	mobpopupopen= 'yes';
 	
