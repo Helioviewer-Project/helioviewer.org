e57ad9c92ad0b535f56ab31a3c02f2875442d8ed
btaylor-sesda <87322123+btaylor-sesda@users.noreply.github.com>
2022-11-02 14:00:42 -0400

    Update responsive_hv.js

--- resources/lib/responsive/responsive_hv.js
+++ resources/lib/responsive/responsive_hv.js
@@ -599,7 +599,7 @@ $(document.body).on('click','.toggle_empty',function(){
 // TESTING: ensure that the recently youtube shared videos are open
 setTimeout(function() {
 	if($('#accordion-youtube .header .disclosure-triangle').hasClass('closed')) {
-		$('#accordion-youtube .header .disclosure-triangle').click();
+		$('#accordion-youtube').click();
 	}
 
 	$('#accordion-youtube .header .disclosure-triangle').removeClass('closed');
