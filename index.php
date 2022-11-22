7dd39d1bc088280b4e0315b99f4cc141af9cdb78
btaylor-sesda <87322123+btaylor-sesda@users.noreply.github.com>
2022-11-22 15:48:40 -0500

    Update index.php

--- index.php
+++ index.php
@@ -444,9 +444,13 @@ echo $hvdesktopcsshides;
 			</tr>
 
 			<tr>
-				<td class="dtcycle_jump_td" colspan="4" style="">
+				<td class="dtcycle_jump_td" colspan="2" style="">
 					<span style="color:white;">JUMP:</span>&nbsp;
 					<span id="hvmobjump_div"></span>
+				</td>
+				
+				<td class="dtcycle_jump_td" colsan="2" >
+					<div id="timeNowBtn_mob">NEWEST</div>
 				</td>
 			</tr>
 
