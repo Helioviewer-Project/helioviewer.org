1ff46377dcce7a639d56f88b75b5e0e43d5270b1
btaylor-sesda <87322123+btaylor-sesda@users.noreply.github.com>
2022-12-22 10:43:51 -0500

    Update Config.js

--- resources/js/Utility/Config.js
+++ resources/js/Utility/Config.js
@@ -1,6 +1,7 @@
 /**
- * @author <a href="mailto:daniel.garciabriseno@nasa.gov">Daniel Garcia Briseno</a>
- * @fileOverview This configuration is meant to work for the pre-built docker environment.
+ * @author <a href="mailto:jeff.stys@nasa.gov">Jeff Stys</a>
+ * @author <a href="mailto:keith.hughitt@nasa.gov">Keith Hughitt</a>
+ * @fileOverview This class handles the creation and validation of basic configuration parameters
  */
 /*jslint browser: true, white: true, onevar: true, undef: true, nomen: false, eqeqeq: true, plusplus: true,
 bitwise: true, regexp: true, strict: true, newcap: true, immed: true, maxlen: 120, sub: true */
@@ -13,28 +14,30 @@ var Config = Class.extend(
      * Default parameters
      */
      params: {
-        'back_end'             	: "http://localhost:8081",
-        'web_root_url'         	: "http://localhost:8080",
-	    'build_num'             : 700,
-        'default_image_scale'   : 4.8408817,
-        'min_image_scale'       : 0.60511022,
-        'max_image_scale'       : 154.90822,
-        'max_tile_layers'       : 5,
-        'prefetch_size'        	: 0,
-        'news_url'             	: "https://blog.helioviewer.org/",
-        'user_video_feed'       : "http://localhost:8081",
-        'contact_email'        	: "contact@helioviewer.org"
+        'back_end'                  : "https://api.helioviewer.org/",
+        'web_root_url'              : "https://helioviewer.org",
+        'build_num'                 : 700,
+        'default_image_scale'       : 4.8408817,
+        'min_image_scale'           : 0.60511022,
+        'max_image_scale'           : 154.90822,
+        'max_tile_layers'           : 5,
+        'prefetch_size'             : 0,
+        'news_url'                  : "https://helioviewer-project.github.io/",
+        'user_video_feed'           : "https://api.helioviewer.org/",
+        'contact_email'             : "HelioViewerDevelopment@nasa.onmicrosoft.com",
+        'regenerate_movie_threshold': 90,
+        'enable_helios_backlinks'   : true
      },
-    
+
     /**
      * @description Creates a new Config.
      * @constructs
      */
     init: function (params) {
         var self = this;
-        
+
         $.each(params, function (k,v) {
-	        self.params[k] = v;
+            self.params[k] = v;
         });
 
         this.bools  = [];
@@ -78,18 +81,20 @@ var Config = Class.extend(
      */
     toArray: function () {
         return {
-            'version'             : this.params["build_num"],
-            'defaultImageScale'   : this.params["default_image_scale"],
-            'minImageScale'       : this.params["min_image_scale"],
-            'maxImageScale'       : this.params["max_image_scale"],
-            'maxTileLayers'       : this.params["max_tile_layers"],
-            'prefetchSize'        : this.params["prefetch_size"],
-            'backEnd'             : this.params["back_end"],
-            'newsURL'             : this.params["news_url"],
-            'rootURL'             : this.params["web_root_url"],
-            'videoFeed'           : this.params["user_video_feed"],
-            'contactEmail'        : this.params["contact_email"],
-            'apiURL'              : this.params["back_end"]
+            'version'                 : this.params["build_num"],
+            'defaultImageScale'       : this.params["default_image_scale"],
+            'minImageScale'           : this.params["min_image_scale"],
+            'maxImageScale'           : this.params["max_image_scale"],
+            'maxTileLayers'           : this.params["max_tile_layers"],
+            'prefetchSize'            : this.params["prefetch_size"],
+            'backEnd'                 : this.params["back_end"],
+            'newsURL'                 : this.params["news_url"],
+            'rootURL'                 : this.params["web_root_url"],
+            'videoFeed'               : this.params["user_video_feed"],
+            'contactEmail'            : this.params["contact_email"],
+            'apiURL'                  : this.params["back_end"],
+            'regenerateMovieThreshold': this.params["regenerate_movie_threshold"],
+            'enableHelios'            : this.params["enable_helios_backlinks"]
         };
     }
 });
