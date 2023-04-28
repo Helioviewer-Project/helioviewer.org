f7564020bcf6157a89ffcf76380d85a15dd73c83
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-04-28 13:39:57 -0400

    Implement tabgroups for views

--- resources/js/Events/EventViewer.js
+++ resources/js/Events/EventViewer.js
@@ -13,20 +13,19 @@ import React, { useState, useEffect } from 'react';
  */
 export default function EventViewer({views, source, onChange}) {
     // Store the currently selected tab, used to index into the views array.
-    const [tab, setTab] = useState(0)
+    const [tab, setTab] = useState(views[0])
     useEffect(onChange);
+    let groups = GetTabGroups(views);
 
     return <div>
         <div className='event-info-dialog-menu'>
             {/* Map each view to into its own unique Tab. When clicked, tab will be updated with the selected index */}
-            {
-                views.map((view, idx) => views[idx].content != null ? <Tab key={idx} name={view.name} selected={idx == tab} onClick={(_) => setTab(idx)} /> : "")
-            }
+            {groups.map((viewGroup, idx) => <TabGroup key={idx} views={viewGroup} selected={tab} setSelected={setTab} />)}
             {/* Then create the final tab on the right containing the "All" text */}
             <Tab key={'all'} name={'all'} extraClasses={'right'} selected={tab == 'all'} onClick={(_) => setTab('all')} />
         </div>
         {/* When a tab is selected, access the view by index. When "all" is selected, use the JsonViewer */}
-        {tab != 'all' ? <JsonViewer value={views[tab].content} /> :
+        {tab != 'all' ? <JsonViewer value={tab.content} /> :
                         <JsonViewer value={source} />}
     </div>;
 }
@@ -39,5 +38,45 @@ export default function EventViewer({views, source, onChange}) {
  * @param {string} extraClasses Extra HTML classes to place in the element
  */
 function Tab({name, selected, onClick, extraClasses}) {
-    return <a className={`show-tags-btn ${extraClasses} ${selected ? 'selected' : ''}`} onClick={onClick}>{name}</a>
+    return <a className={`show-tags-btn ${extraClasses || ''} ${selected ? 'selected' : ''}`} onClick={onClick}>{name}</a>
+}
+
+/**
+ * Renders tabs within a tabgroup
+ * @param {Array<View>} views List of views that are in this tabgroup
+ * @param {View} selected Selected view
+ * @param {function} setSelected function to set the selected tab identifier
+ */
+function TabGroup({views, selected, setSelected}) {
+    let selectedKey = window.btoa(JSON.stringify(selected));
+    return <div className='tabgroup'>
+        {
+        views.map((view, idx) => {
+            let key = window.btoa(JSON.stringify(view));
+            return views[idx].content != null ? <Tab tabgroup={view.tabgroup} key={key} name={view.name} selected={selectedKey == key} onClick={(_) => setSelected(view)} /> : ""
+        })
+        }
+    </div>
+}
+
+/**
+ * Parses the list of views into groups based on their tabgroup field.
+ * Lower tabgroups will appear before
+ * @param {Array<Array<View>>} views
+ */
+function GetTabGroups(views) {
+    let groups = {};
+    // This group is used if there is no specific tabgroup specified.
+    // Which means the view is its own unique tab.
+    let groupIndex = 99999;
+    views.forEach((view) => {
+        // Use the specified tabgroup. If one is not specified, then get a unique tabgroup id
+        let tabgroup = view.tabgroup || groupIndex++;
+        if (!groups.hasOwnProperty(tabgroup)) {
+            groups[tabgroup] = [view];
+        } else {
+            groups[tabgroup].push(view);
+        }
+    })
+    return Object.values(groups).reverse();
 }
