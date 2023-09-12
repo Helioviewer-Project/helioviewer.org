67b469aa6cd42b15bfb15ff0b57b4a1db4b93ae3
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-09-12 11:21:43 -0400

    Implement improved coordinate calculation (#487)
    Corrects for Helioviewer's constant 1AU limitation and handles rotated images.
    
    Images in HV should be rotated to have the north pole at the top, but this isn't true for solar orbiter at this time.

--- resources/js/Viewport/Helper/MouseCoordinates.js
+++ resources/js/Viewport/Helper/MouseCoordinates.js
@@ -17,6 +17,7 @@ var MouseCoordinates = Class.extend(
     init: function (imageScale, showMouseCoordsWarning) {
         this.imageScale      = imageScale;
         this.warnMouseCoords = showMouseCoordsWarning;
+        this._ready = false;
 
         this.viewportContainer = $('#helioviewer-viewport').parent();
         this.movingContainer   = $("#moving-container");
@@ -26,6 +27,7 @@ var MouseCoordinates = Class.extend(
         this.mouseCoords  = "disabled";
         this.mouseCoordsX = $('#mouse-coords-x');
         this.mouseCoordsY = $('#mouse-coords-y');
+        $(document).bind("helioviewer-ready", () => this._ready = true);
     },
 
     enable: function () {
@@ -49,8 +51,8 @@ var MouseCoordinates = Class.extend(
         var offset = this.viewportContainer.offset();
 
         return {
-            x: screenx - offset.left - 1,
-            y: screeny - offset.top - 1
+            x: screenx - offset.left,
+            y: screeny - offset.top
         };
     },
 
@@ -73,19 +75,6 @@ var MouseCoordinates = Class.extend(
         } else {
             $(document).unbind('mousemove', this.updateMouseCoords);
             //this.movingContainer.unbind('mousemove', this.updateMouseCoords);
-        }
-    },
-
-    /**
-     * Checks to see whether a warning message should be displayed to the user
-     */
-    _checkWarning: function () {
-        // Warn once
-        if (this.warnMouseCoords === true) {
-            var warning = "<b>Note:</b> Mouse coordinates should not be used for operations.";
-            $(document).trigger("message-console-log", [warning, {"sticky": false, "life": 20000}]);
-            Helioviewer.userSettings.set("notifications.coordinates", false);
-            this.warnMouseCoords = false;
         }
     },
 
@@ -123,13 +112,86 @@ var MouseCoordinates = Class.extend(
         //scale
         let zoom = (Helioviewer.userSettings.get('mobileZoomScale') || 1);
         scale = this.imageScale / zoom;
-        x = Math.round((scale * MX.x));
-        y = - Math.round((scale * MX.y));
+        // TODO: Apply scaling fix depending on the current layer
+        x = scale * MX.x;
+        y = scale * MX.y;
+        let correctedCoord = this.correctCoordinate(this.imageScale, MX.x, -MX.y)
 
         // Return scaled coords
         return {
-            x: x,
-            y: y
+            x: Math.round(correctedCoord.x),
+            y: Math.round(correctedCoord.y)
         };
+    },
+
+    /**
+     * Returns the metadata for the image layer that the mouse is currently hovering over.
+     *
+     * This metadata can be used to compute the correct projective coordinate from the pixel coordinate.
+     * @returns {Object|null} The image layer or null if the mouse is not over an image.
+     */
+    getImageLayer: function (x, y) {
+        // The helioviewer instance needs to be set on the page before it can be used.
+        // An event is sent when the instance is ready and that is when _ready will be true.
+        if (this._ready) {
+            let layers = helioviewer.viewport.getVisibleLayerInstances({
+                left: x,
+                right: x,
+                top: y,
+                bottom: y
+            });
+            // Choose the top layer under the mouse and apply the scale correction for it.
+            let keys = Object.keys(layers);
+            if (keys.length > 0) {
+                return layers[keys[0]];
+            }
+        }
+        return null;
+    },
+
+    correctCoordinate: function (scale, x, y) {
+        // Get the layer that the mouse is hovering over.
+        let layer = this.getImageLayer(x, y);
+        let coord = this.correctScale(layer, scale, x, y);
+        return this.correctRotation(layer, coord.x, coord.y)
+    },
+
+    /**
+     * Helioviewer's scaling method assumes a constant sun-earth distance, which leads to an incorrect calculation for coordinates.
+     * This function corrects for this assumption based on the current layer the mouse is hovering over.
+     * @param {Object} layer Layer metadata
+     * @param {number} scale Desired image scale
+     * @param {number} x x position (pixels)
+     * @param {number} y y position (pixels)
+     * @returns {Coordinate} coordinates in arcseconds
+     *
+     * @typedef {Object} Coordinate
+     * @property {number} x X coordinate
+     * @property {number} y Y Coordinate
+     */
+    correctScale: function (layer, scale, x, y) {
+        let correction = layer ? layer.image.scaleCorrection : 1;
+        // image.scaleCorrection is a property returned by the getClosestImage API.
+        scale *= correction;
+        return {
+            x: scale * x,
+            y: scale * y
+        };
+    },
+
+    correctRotation: function (layer, x, y) {
+        let angle = layer ? layer.image.rotation : 0;
+        let theta_rad = angle * Math.PI / 180;
+        // Assumption here is that the image scale is the same for the x and y axes.
+        // This equation is pulled from Thompson (2006) section 8. The older FITS coordinate system
+        let cos = Math.cos(theta_rad);
+        let sin = Math.sin(theta_rad);
+        let rotated_x = cos * x - sin * y;
+        let rotated_y = sin * x + cos * y;
+
+        return {
+            x: rotated_x,
+            y: rotated_y
+        }
     }
 });
