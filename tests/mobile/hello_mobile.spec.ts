1cbaab1cdf90a0ae2e1e1f1d59953c5ece8ba103
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2024-05-08 14:14:10 -0400

    Update locator

--- tests/mobile/hello_mobile.spec.ts
+++ tests/mobile/hello_mobile.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('Mobile - Displays initial AIA 304 Image', async ({ page }) => {
   await page.goto('http://localhost:8080/');
-  await page.getByRole('cell', { name: 'NEWEST' }).locator('span').click();
+  await page.locator('.hvbottombar').getByText('NEWEST').click();
   // TODO: Mobile doesn't have a loading indicator we can wait on.
   await page.waitForTimeout(5000);
   await expect(page).toHaveScreenshot();
