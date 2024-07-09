7a4a8a31681c02a5374cb3e258e21de66df837b3
Kasim Necdet Percinel <kasim.n.percinel@nasa.gov>
2024-07-09 15:03:44 +0000

    clean notifications with while loop, and add path for url

--- tests/page_objects/helioviewer.ts
+++ tests/page_objects/helioviewer.ts
@@ -24,8 +24,8 @@ class Helioviewer {
         this.sidebar = this.page.locator('#hv-drawer-left');
     }
 
-    async Load() {
-        await this.page.goto('/');
+    async Load(path = '/') {
+        await this.page.goto(path);
         await this.WaitForLoadingComplete();
     }
 
@@ -86,13 +86,10 @@ class Helioviewer {
     }
 
     async CloseAllNotifications() {
-        let close_buttons = await this.page.locator('.jGrowl-close');
-        let count = await close_buttons.count()
-        for (let n = 0; n < count; n++) {
-            await close_buttons.nth(n).click();
+        while ((await this.page.locator('.jGrowl-close').count()) > 0) {
+            await this.page.locator('.jGrowl-close').first().click();
+            await this.page.waitForTimeout(1000);
         }
-        // Wait for notifications to disappear
-        await this.page.waitForTimeout(1000);
     }
 
     /**
