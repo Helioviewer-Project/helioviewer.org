80ebd5441777e7517a3ce642f77d4ee45a329aeb
Kasim Necdet Percinel <kasim.n.percinel@nasa.gov>
2024-07-12 18:08:51 +0000

    split movie tests and move movie snapsnots

---
+++ tests/desktop/normal/movie/movie.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+import { Helioviewer } from '../../../page_objects/helioviewer'
+import * as fs from 'fs';
+
+
+// If there is no image to make movie , then hv should show some friendly notification message
+test('If there is not enough data, helioviewer should show error message about no data situation', async ({ page }) => {
+
+  let hv = new Helioviewer(page);
+
+  // load helioviewer
+  // Action 1 : BROWSE TO HELIOVIEWER
+  await hv.Load();
+  await hv.CloseAllNotifications();
+
+  // Action 2 : SUBMIT FULLSCREEN MOVIE
+  await hv.movie.makeFullScreenmovie();
+
+  await hv.assertNotification('error', 'No images found for requested time range. Please try a different time.');
+ 
+})
