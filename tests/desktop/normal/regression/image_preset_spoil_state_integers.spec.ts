308db1a5f3fd3835912df8fa007fdaba25cc2d23
Kasim Necdet Percinel <kasim.n.percinel@nasa.gov>
2024-07-16 12:28:37 -0400

    Fix user presets spoil state integer variables  (#583)
    * edit userpresets for correct dates
    
    * added string to integer function to fix integer types
    
    * added regression tests for the bug

---
+++ tests/desktop/normal/regression/image_preset_spoil_state_integers.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+import { Helioviewer } from '../../../page_objects/helioviewer';
+import * as fs from 'fs';
+
+/**
+ * This test will make Helioviewer load the default AIA 304 image that is
+ * embedded in the development container and perform a visual comparison to make
+ * sure the page appears how we expect it to appear.
+ */
+test('Selecting image presets should not spoil integer state variables', async ({ page }) => {
+
+  let hv = new Helioviewer(page);
+
+  // load helioviewer
+  // Action 1 : BROWSE TO HELIOVIEWER
+  await hv.Load();
+  await hv.CloseAllNotifications();
+
+  // Action 2 : Open left sources panel
+  await hv.ClickDataSourcesTab();
+
+  // Action 3: Load preset Eruption Monitor
+  await hv.SelectImagePreset('Eruption Monitor');
+  await hv.CloseAllNotifications();
+
+  // Action 4: Try to share screen
+  await hv.urlshare.triggerShareURL();
+
+  // Action 5: Check if the shared url is prepared
+  await hv.urlshare.sharedURLIsVisibleAndDone();
+
+});
