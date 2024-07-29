62db17f7d9a5a7241da1e3bfc6b103de1e4f5217
Kasim Necdet Percinel <kasim.n.percinel@nasa.gov>
2024-07-29 14:22:33 +0000

    vso sunpy download button tests

---
+++ tests/desktop/normal/vso/drawer_should_open_and_close.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+import { Helioviewer } from '../../../page_objects/helioviewer';
+import * as fs from 'fs';
+
+/**
+ * This test checks drawer open/close functionality
+ */
+test('validate drawer should open and close the required tab', async ({ page }) => {
+
+    let hv = new Helioviewer(page);
+
+    // load helioviewer
+    // Action 1 : BROWSE TO HELIOVIEWER
+    await hv.Load();
+    await hv.CloseAllNotifications();
+
+    // Action 2 : Open data drawer
+    await hv.vso_drawer.toggleVisibility();
+
+    // Check drawer is open
+    hv.vso_drawer.assertDrawerOpen();
+
+    // Action 3 : Close drawer
+    await hv.vso_drawer.toggleVisibility();
+
+    // Check drawer is closed
+    hv.vso_drawer.assertDrawerClose();
+
+    // Action 4 : Open again
+    await hv.vso_drawer.toggleVisibility();
+
+    // Check drawer is open again
+    hv.vso_drawer.assertDrawerOpen();
+})
