564f5762eb54748a1c0d5a7acc37954c7ed88a48
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2024-08-02 11:28:50 -0400

    Fix duplicated "No shared movies found"

--- resources/js/UI/UserVideoGallery.js
+++ resources/js/UI/UserVideoGallery.js
@@ -12,7 +12,7 @@ newcap: true, immed: true, maxlen: 80, sub: true */
 var UserVideoGallery = Class.extend(
     /** @lends UserVideoGallery.prototype */
     {
-	observationDateMovies: {},    
+	observationDateMovies: {},
     /**
      * @constructs
      * @description Creates a new UserVideoGallery component
@@ -20,11 +20,12 @@ var UserVideoGallery = Class.extend(
     init : function (url) {
         this._container   			= $("#user-video-gallery-main");
         this._containerCurrent   	= $("#user-video-gallery-main-current");
+        this._noSharedMoviesFound   = $("#user-video-gallery-current .js-no-movies");
         this._loader      			= $("#user-video-gallery-spinner");
         this._loaderCurrent      	= $("#user-video-gallery-spinner-current");
 
         this._working     = false;
-		
+
 		//create empty container
 		$('<div id="movies-container"></div>').appendTo("#moving-container");
 		this._moviesContainer      	= $("#movies-container");
@@ -49,26 +50,26 @@ var UserVideoGallery = Class.extend(
 		        self._checkForNewMovies();
 	        }
         }, 120000);
-        
+
         $(document).on('observation-time-changed', function(e){
 	        if(Helioviewer.userSettings.get("state.drawers.#hv-drawer-youtube.open") || Helioviewer.userSettings.get("options.showinviewport")){
 	        	self._fetchCurrentVideos();
-	        }	
+	        }
         });
-        
+
         $('#user-video-gallery-current').on('click', '.user-video-label', function(){
 	        var rel = $(this).data('time');
 	        helioviewerWebClient.timeControls.setDate( new Date(rel).toUTCDate() );
         });
-        
+
         $('#user-video-gallery-current').on('click', '.user-video-current-show-more', function(){
 	        $(this).remove();
 	        self._containerCurrent.append(self._loaderCurrent);
 	        self._fetchCurrentVideosShowMore();
         });
-        
+
         $(document).bind("image-scale-changed", $.proxy(this.changeScale, this));
-        
+
         //Show in viewport button initialization
         if(Helioviewer.userSettings.get("options.showinviewport")){
 	        $('#movies-show-in-viewport').prop( "checked", true );
@@ -77,13 +78,13 @@ var UserVideoGallery = Class.extend(
 	        $('#movies-show-in-viewport').prop( "checked", false );
 	        this._moviesContainer.hide();
         }
-        
+
         $('#movies-show-in-viewport').change(function(){
-	        if($(this).is(":checked")) { 
+	        if($(this).is(":checked")) {
 	            Helioviewer.userSettings.set("options.showinviewport", true);
 	            self._moviesContainer.show();
-	        }else{ 
-	            Helioviewer.userSettings.set("options.showinviewport", false); 
+	        }else{
+	            Helioviewer.userSettings.set("options.showinviewport", false);
 	            self._moviesContainer.hide();
 	        }
 	    });
@@ -97,7 +98,7 @@ var UserVideoGallery = Class.extend(
         if(this._lastTimeCurrentVideosUpdated == parseInt(Helioviewer.userSettings.get("state.date"))){
 	        return;
         }
-        
+
         // Query parameters
         this._lastTimeCurrentVideosUpdated = parseInt(Helioviewer.userSettings.get("state.date"));
         var params = {
@@ -108,7 +109,7 @@ var UserVideoGallery = Class.extend(
         $('.user-video-thumbnail-container-current').remove();
         $('.user-video-current-show-more').remove();
         this._containerCurrent.find("p, div").remove();
-        
+
         // Show loading indicator
         this._loaderCurrent.show();
 
@@ -130,7 +131,7 @@ var UserVideoGallery = Class.extend(
             "skip"   : $('.user-video-thumbnail-container-current').length,
             "date"   : new Date(this._lastTimeCurrentVideosUpdated).toISOString()
         };
-        
+
         // Show loading indicator
         this._loaderCurrent.show();
 
@@ -220,24 +221,24 @@ var UserVideoGallery = Class.extend(
      */
     _buildHTML: function (videos) {
         var html = "", self = this, count = 0;
-		
+
 		this._loader.hide();
-		
+
         $.each(videos, function (i, vid) {
             var when = new Date.parseUTCDate(vid.published) .getElapsedTime() + ' ago', img = vid.thumbnails['small'], html = '';
-			
+
 			html = "<div class='user-video-thumbnail-container user-video-thumbnail-container-recent'>\
                     	<a target='_blank' href='" + vid.url + "' alt='video thumbnail' id='youtube-movie-current-"+vid.id+"'>\
 							<img class='user-video-thumbnail' src='" + img + "' alt='user video thumbnail' />\
                     	</a>\
                     	<div style='text-align: center;'>" + when + "</div>\
                     </div>";
-                    
+
             // Drop tailing line break
-			html = html.slice(0, -6);  
-			
+			html = html.slice(0, -6);
+
 			self._container.append(html);
-			
+
 			// Create a preview tooltip
 			$("#youtube-movie-" + vid.id).qtip({
 	            content: {
@@ -258,12 +259,14 @@ var UserVideoGallery = Class.extend(
 	                delay: 140
 	            }
 	        });
-	        count++;    
+	        count++;
         });
-        
+
 		if($('.user-video-thumbnail-container-recent').length == 0){
-			this._containerCurrent.append('<p>No shared movies found.</p>');
-		}
+            this._noSharedMoviesFound.show();
+		} else {
+            this._noSharedMoviesFound.hide();
+        }
 
         this._working = false;
     },
@@ -273,18 +276,18 @@ var UserVideoGallery = Class.extend(
      */
     _buildHTMLCurrent: function (videos) {
         var html = "", self = this, count = 0;
-		
+
 		this._loaderCurrent.hide();
 		var currentScale = Helioviewer.userSettings.get("state.imageScale");
 		self._moviesContainer.html('');
-		
+
         $.each(videos, function (i, vid) {
             var img = vid.thumbnails['small'];
-			
+
 			var videoStartDate = new Date(vid.startDate);
 			var videoEndDate = new Date(vid.endDate);
 			var videoMiddleDate = new Date((videoStartDate.getTime() + videoEndDate.getTime()) / 2);
-			
+
 			//Viewport movie box
 			var iconTop = ((vid.roi.top + vid.roi.bottom)/2) / currentScale * vid.imageScale;
 			var iconLeft = ((vid.roi.right + vid.roi.left)/2) / currentScale * vid.imageScale;
@@ -294,30 +297,17 @@ var UserVideoGallery = Class.extend(
 			var boxWidth = vid.roi.width / currentScale * vid.imageScale;
 			var title = vid.keywords;
 			title = vid.title.substring(0, vid.title.indexOf('('));
-			
+
 			var vidTitle = '<span class="qtip-left user-video-label" data-time="'+videoMiddleDate.getTime()+'" title="Set observation date to '+videoMiddleDate.toDateString()+' '+videoMiddleDate.toTimeString()+' UTC">'+title+' ('+vid.startDate+' '+vid.endDate+'</span>'
 							+' <span class="dateSelector" data-tip-pisition="left" data-date-time="'+vid.startDate+'"'
 							+' data-date-time-end="'+vid.endDate+'">UTC</span>)';
-			
+
 			self._moviesContainer.append('<a id="youtube-movie-viewport-icon-'+vid.id+'" class="movie-viewport-icon event-label"\
 					href="'+ vid.url +'" target="_blank"\
 					data-id="'+vid.id+'" data-scale="'+vid.imageScale+'" data-top="'+((vid.roi.top + vid.roi.bottom)/2)+'" data-left="'+((vid.roi.right + vid.roi.left)/2)+'" \
 					style="left: '+iconLeft+'px;top: '+iconTop+'px;"><i class="fa fa-video-camera"></i> '+title+'\
 				</a>');
-            //Movie border box
-            /*self._moviesContainer.append('<div id="youtube-movie-viewport-box-'+vid.id+'" \
-					class="movie-viewport-box" \
-					data-id="'+vid.id+'" data-scale="'+vid.imageScale+'" data-top="'+vid.roi.top+'" data-left="'+vid.roi.left+'" data-height="'+vid.roi.height+'" data-width="'+vid.roi.width+'"\
-					style="left: '+boxLeft+'px;top: '+boxTop+'px;height: '+boxHeight+'px;width: '+boxWidth+'px;">\
-				</div>');
-            
-            $('#youtube-movie-viewport-icon-'+vid.id).hover(function(){
-	            $('#youtube-movie-viewport-box-'+vid.id).show();
-            },function(){
-	            $('#youtube-movie-viewport-box-'+vid.id).hide();
-            });*/
-            
-            
+
 	        $("#youtube-movie-viewport-icon-" + vid.id).qtip({
 	            content: {
 	                title: {
@@ -338,20 +328,20 @@ var UserVideoGallery = Class.extend(
 				    }
 				}
 	        });
-			
+
             html = "<div class='user-video-thumbnail-container user-video-thumbnail-container-current' data-id='"+vid.id+"'>\
                     	<a target='_blank' href='" + vid.url + "' " + "alt='video thumbnail' id='youtube-movie-current-"+vid.id+"'>\
 							<img class='user-video-thumbnail' src='" + img + "' alt='user video thumbnail' />\
                     	</a>\
                     	<div style='text-align: center;padding:0px 10px; cursor:pointer' >" + vidTitle + "</div>\
                     </div>";
-            
-                    
+
+
             // Drop tailing line break
-			html = html.slice(0, -6);  
-			
+			html = html.slice(0, -6);
+
 			self._containerCurrent.append(html);
-			
+
 			// Create a preview tooltip
 			$("#youtube-movie-current-" + vid.id).qtip({
 	            content: {
@@ -374,17 +364,20 @@ var UserVideoGallery = Class.extend(
 	        });
 	        count++;
         });
-        
+
         if(typeof helioviewerWebClient !== 'undefined'){
 	        helioviewerWebClient._timeSelector = new TimeSelector();
         }
-        
+
 		if($('.user-video-thumbnail-container-current').length == 0){
-			this._containerCurrent.append('<p><br/>No shared movies found.</p>');
-		}else if(count == this._numVideosCurrent){
-			this._containerCurrent.append('<span class="user-video-current-show-more">SHOW MORE</span>');
-		}
-	    
+            this._noSharedMoviesFound.show();
+		} else {
+            this._noSharedMoviesFound.hide();
+            if(count == this._numVideosCurrent){
+                this._containerCurrent.append('<span class="user-video-current-show-more">SHOW MORE</span>');
+            }
+        }
+
 	    $('#user-video-gallery-main-current > div').off();
 	    $('#user-video-gallery-main-current > div').hover(function(){
             var id = $(this).data('id');
@@ -397,7 +390,7 @@ var UserVideoGallery = Class.extend(
             $('.event-marker').show();
             $('.event-region').show();
         });
-		
+
         this._working = false;
     },
 
@@ -418,12 +411,12 @@ var UserVideoGallery = Class.extend(
      */
     _buildPreviewTooltipHTML: function (movie) {
         var width, height, thumbnail, html = "";
-		
+
 		var imageScale = parseFloat(movie.imageScale);
-		
+
         html += "<div style='text-align: center;'>" +
 	            	"<img src='" + movie.thumbnails.medium + "' width='95%' alt='preview thumbnail' />" +
-	            "</div>" + 
+	            "</div>" +
 	            "<table class='preview-tooltip'>" +
 	            	"<tr><td><b>Start:</b></td><td>" + movie.startDate + "</td></tr>" +
 					"<tr><td><b>End:</b></td><td>"   + movie.endDate   + "</td></tr>" +
@@ -433,36 +426,18 @@ var UserVideoGallery = Class.extend(
 
         return html;
     },
-	
+
 	changeScale: function (event, imageScale) {
-        
         $('.movie-viewport-icon').each(function(i, obj){
 	        var id = $(obj).data('id');
 	        var scale = $(obj).data('scale');
 	        var iconTop = $(obj).data('top') / imageScale * scale;
 			var iconLeft = $(obj).data('left') / imageScale * scale;
-			
+
 			$('#youtube-movie-viewport-icon-'+id).css({
 				top: iconTop + 'px',
 				left: iconLeft + 'px'
 			});
         });
-        
-        /*$('.movie-viewport-box').each(function(i, obj){
-	        var id = $(obj).data('id');
-	        var scale = $(obj).data('scale');
-			var boxTop = $(obj).data('top') / imageScale * scale;
-			var boxLeft = $(obj).data('left') / imageScale * scale;
-			var boxHeight = $(obj).data('height') / imageScale * scale;
-			var boxWidth = $(obj).data('width') / imageScale * scale;
-			
-			$('#youtube-movie-viewport-box-'+id).css({
-				top: boxTop + 'px',
-				left: boxLeft + 'px',
-				height: boxHeight + 'px',
-				width: boxWidth + 'px'
-			});
-        });*/
-        
     }
 });
