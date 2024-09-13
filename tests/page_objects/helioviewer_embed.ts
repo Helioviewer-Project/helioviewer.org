61e55a4e5ac5f91e25b4a00f2c5bf12cdee48c1b
Kasim Necdet Percinel <kasim.n.percinel@nasa.gov>
2024-09-13 21:26:53 +0000

    fix forgotten inclusion

--- tests/page_objects/helioviewer_embed.ts
+++ tests/page_objects/helioviewer_embed.ts
@@ -1,4 +1,4 @@
-import { expect, Page } from '@playwright/test';
+import { expect, Page, Info } from '@playwright/test';
 import { Helioviewer } from './helioviewer';
 import { HelioviewerInterface } from './helioviewer_interface';
 
