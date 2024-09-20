25a0e068de5e97f92f12238eed7d57bf5d81b298
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2024-09-20 10:32:46 -0400

    Add Viewport Tests (and other test patches) (#615)
    * Repair screenshot.spec.ts
    
    * Add missing awaits
    
    * rm test.only
    
    * Add WaitForLoad before making assertions in difference.spec.ts
    
    * rm test.only
    
    * update doc comments
    
    * Update screenshot test to mask "seconds created"
    
    * Add mobile center-viewport test
    
    * Update mobile_hv page object
    
    * test.only
    
    * Update snapshots
    
    * Update screenshot test snapshot
    
    * Use more robust method of waiting for zoom to finish
    
    * Update test snapshots
    
    * Mark viewport test as flaky
    
    * Let minimal use hv.Load
    
    * Fix zooming in viewport test
    
    * Add linux snapshots
    
    * Fix test.only
    
    * Update test snapshot
    
    * Update snapshot style
    
    * update safari snapshot
    
    * Update first-screenshot-created to have mask
    
    * What's wrong with safari?
    
    * Come on safari snapshot, you can do it
    
    * Update screenshot snap to have mask
    
    * Disable fast-fail in tests
    
    * fast-fail fail-fast, it's one of those
    
    * Update second screenshot mask
    
    * Update firefox screenshot mask
    
    * Uncomment assertion

--- tests/desktop/minimal/hello_minimal.spec.ts
+++ tests/desktop/minimal/hello_minimal.spec.ts
@@ -1,4 +1,5 @@
 import { test } from '@playwright/test';
+import { Helioviewer } from '../../page_objects/helioviewer';
 
 /**
  * This test will make Helioviewer load the default AIA 304 image that is
@@ -6,9 +7,9 @@ import { test } from '@playwright/test';
  * sure the page appears how we expect it to appear.
  */
 test('Displays initial AIA 304 Image', async ({ page }) => {
-  await page.goto('/?output=minimal');
+  let hv = new Helioviewer(page);
   // Wait for page to load
-  await page.waitForTimeout(5000);
+  await hv.Load("/?output=minimal");
   await page.getByLabel('Date').click();
   await page.getByLabel('Date').fill('2021/06/01');
   await page.getByLabel('Date').press('Tab');
