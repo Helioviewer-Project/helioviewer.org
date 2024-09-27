73599b9734c14a242dcaa95be9b88df1923d940c
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2024-09-27 14:15:36 -0400

    Prettier tests (#624)
    * Run prettier on test files
    
    * Update CI
    
    * Revert "Run prettier on test files"
    
    This reverts commit 88286114485e7f1d156c847d622c94f92226f293.
    
    * Rerun prettier on tests
    
    * Add prettier configuration
    
    * Revert "Rerun prettier on tests"
    
    This reverts commit d6cbc35b53b8b8e1f60989fe5cedbdc238414897.
    
    * Update config
    
    * Rerun prettier with config
    
    * Add other files being prettier'd

--- resources/js/test/patch_broken_screenshots.test.js
+++ resources/js/test/patch_broken_screenshots.test.js
@@ -5,16 +5,14 @@
  * This unit test tests the code which notifies the user that they were affected and broken links were removed.
  */
 
-const {
-  _BrokenScreenshotNotifier,
-} = require("../Patches/broken_screenshots_493.js");
+const { _BrokenScreenshotNotifier } = require("../Patches/broken_screenshots_493.js");
 
 // Mock global Helioviewer object for testing
 global.Helioviewer = {
   userSettings: {
     get: jest.fn(),
-    set: jest.fn(),
-  },
+    set: jest.fn()
+  }
 };
 // Mock jquery trigger
 let trigger = jest.fn();
@@ -27,60 +25,35 @@ global.document = null;
 describe("Broken screenshot patch", () => {
   it("Should count the number of broken screenshots in the list", () => {
     // Set test data to be returned to have one valid and one invalid screenshot
-    Helioviewer.userSettings.get.mockReturnValueOnce([
-      { id: 1 },
-      { id: false },
-    ]);
+    Helioviewer.userSettings.get.mockReturnValueOnce([{ id: 1 }, { id: false }]);
     // Expect the patch to count the 1 invalid screenshot correctly
     let patch = new _BrokenScreenshotNotifier();
     expect(patch.num_broken_screenshots).toBe(1);
 
     // Repeat the test with 3 invalid screenshots
-    Helioviewer.userSettings.get.mockReturnValueOnce([
-      { id: false },
-      { id: false },
-      { id: false },
-    ]);
+    Helioviewer.userSettings.get.mockReturnValueOnce([{ id: false }, { id: false }, { id: false }]);
     patch = new _BrokenScreenshotNotifier();
     expect(patch.num_broken_screenshots).toBe(3);
 
     // Repeat the test with no invalid screenshots
-    Helioviewer.userSettings.get.mockReturnValueOnce([
-      { id: 0 },
-      { id: 1 },
-      { id: 2 },
-    ]);
+    Helioviewer.userSettings.get.mockReturnValueOnce([{ id: 0 }, { id: 1 }, { id: 2 }]);
     patch = new _BrokenScreenshotNotifier();
     expect(patch.num_broken_screenshots).toBe(0);
   });
 
   it("Should remove broken screenshots from the history list", () => {
     // Set test data to 3 invalid screenshots and one valid
-    Helioviewer.userSettings.get.mockReturnValueOnce([
-      { id: false },
-      { id: false },
-      { id: false },
-      { id: 1337 },
-    ]);
+    Helioviewer.userSettings.get.mockReturnValueOnce([{ id: false }, { id: false }, { id: false }, { id: 1337 }]);
     let patch = new _BrokenScreenshotNotifier();
     patch.Apply();
     // Expect the patch to set the screenshots list to the one valid screenshot
     expect(Helioviewer.userSettings.set.mock.calls.length).toBe(1);
-    expect(Helioviewer.userSettings.set.mock.calls[0][0]).toBe(
-      "history.screenshots",
-    );
-    expect(Helioviewer.userSettings.set.mock.calls[0][1]).toStrictEqual([
-      { id: 1337 },
-    ]);
+    expect(Helioviewer.userSettings.set.mock.calls[0][0]).toBe("history.screenshots");
+    expect(Helioviewer.userSettings.set.mock.calls[0][1]).toStrictEqual([{ id: 1337 }]);
   });
 
   it("Should notify the user that N items were removed from their screenshot list", () => {
-    Helioviewer.userSettings.get.mockReturnValueOnce([
-      { id: false },
-      { id: false },
-      { id: false },
-      { id: 1337 },
-    ]);
+    Helioviewer.userSettings.get.mockReturnValueOnce([{ id: false }, { id: false }, { id: false }, { id: 1337 }]);
     let patch = new _BrokenScreenshotNotifier();
     patch.NotifyIfAffected();
     // Expect a call to schedule the jgrowl notification via bind
@@ -90,8 +63,6 @@ describe("Broken screenshot patch", () => {
     $().bind.mock.calls[0][1]();
     expect($().trigger.mock.calls.length).toBe(1);
     expect($().trigger.mock.calls[0][0]).toBe("message-console-log");
-    expect($().trigger.mock.calls[0][1][0]).toContain(
-      "3 of your recent screenshots",
-    );
+    expect($().trigger.mock.calls[0][1][0]).toContain("3 of your recent screenshots");
   });
 });
