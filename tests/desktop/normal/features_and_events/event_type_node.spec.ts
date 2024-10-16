f41d841e00a1b58034350f58a73afccef004be4a
Kasim Necdet Percinel <kasim.n.percinel@nasa.gov>
2024-10-16 22:58:35 +0000

    code style fixes

--- tests/desktop/normal/features_and_events/event_type_node.spec.ts
+++ tests/desktop/normal/features_and_events/event_type_node.spec.ts
@@ -4,14 +4,14 @@ import { mockEvents } from "../../../utils/events";
 
 /**
  * This test mocks some random events for CCMC
- * then selects some specific event_type, 
+ * then selects some specific event_type,
  * then asserts all of the childnodes of event_tree , should be checked,
  * also asserts all of the other nodes, should be unchecked
  */
 test("Checked event type should check all of its child frms and event_instances", async ({ page, browser }, info) => {
   // mocked event data
   const events = {
-    CCMC : {
+    CCMC: {
       et1: {
         et1frm1: {
           et1frm1ei1: {},
@@ -28,7 +28,7 @@ test("Checked event type should check all of its child frms and event_instances"
           et2frm2ei1: {},
           et2frm2ei2: {}
         }
-      },
+      }
     }
   };
 
@@ -51,7 +51,7 @@ test("Checked event type should check all of its child frms and event_instances"
   // Action 3 : Check et2
   await ccmc.toggleCheckEventType("et2");
 
-  // Action 4: Assert all event markers of et2 to be visible, 
+  // Action 4: Assert all event markers of et2 to be visible,
   // also et1 not to be visible
   await ccmc.assertEventVisible("et2frm1ei1");
   await ccmc.assertEventVisible("et2frm1ei2");
@@ -63,49 +63,49 @@ test("Checked event type should check all of its child frms and event_instances"
   await ccmc.assertEventNotVisible("et1frm1ei2");
 
   // Action 5: Open up all frm branches
-  await ccmc.toggleBranchFRM("et1","et1frm1");
-  await ccmc.toggleBranchFRM("et2","et2frm1");
-  await ccmc.toggleBranchFRM("et2","et2frm2");
+  await ccmc.toggleBranchFRM("et1", "et1frm1");
+  await ccmc.toggleBranchFRM("et2", "et2frm1");
+  await ccmc.toggleBranchFRM("et2", "et2frm2");
 
   // Action 6: Assert which frm and eventtype nodes are checked
   await ccmc.assertEventTypeNodeChecked("et2");
-  await ccmc.assertFrmNodeChecked("et2","et2frm1");
-  await ccmc.assertFrmNodeChecked("et2","et2frm2");
-  await ccmc.assertEventInstanceNodeChecked("et2","et2frm1","et2frm1ei1");
-  await ccmc.assertEventInstanceNodeChecked("et2","et2frm1","et2frm1ei2");
-  await ccmc.assertEventInstanceNodeChecked("et2","et2frm1","et2frm1ei3");
-  await ccmc.assertEventInstanceNodeChecked("et2","et2frm2","et2frm2ei1");
-  await ccmc.assertEventInstanceNodeChecked("et2","et2frm2","et2frm2ei2");
+  await ccmc.assertFrmNodeChecked("et2", "et2frm1");
+  await ccmc.assertFrmNodeChecked("et2", "et2frm2");
+  await ccmc.assertEventInstanceNodeChecked("et2", "et2frm1", "et2frm1ei1");
+  await ccmc.assertEventInstanceNodeChecked("et2", "et2frm1", "et2frm1ei2");
+  await ccmc.assertEventInstanceNodeChecked("et2", "et2frm1", "et2frm1ei3");
+  await ccmc.assertEventInstanceNodeChecked("et2", "et2frm2", "et2frm2ei1");
+  await ccmc.assertEventInstanceNodeChecked("et2", "et2frm2", "et2frm2ei2");
 
   // Action 7: Assert which frm and eventtype nodes are unchecked
   await ccmc.assertEventTypeNodeUnchecked("et1");
-  await ccmc.assertFrmNodeUnchecked("et1","et1frm1");
-  await ccmc.assertEventInstanceNodeUnchecked("et1","et1frm1","et1frm1ei1");
-  await ccmc.assertEventInstanceNodeUnchecked("et1","et1frm1","et1frm1ei2");
+  await ccmc.assertFrmNodeUnchecked("et1", "et1frm1");
+  await ccmc.assertEventInstanceNodeUnchecked("et1", "et1frm1", "et1frm1ei1");
+  await ccmc.assertEventInstanceNodeUnchecked("et1", "et1frm1", "et1frm1ei2");
 
   // Action 8 : Toggle again to uncheck all child nodes
   await ccmc.toggleCheckEventType("et2");
 
   // Action 9: Assert et2 nodes are unchecked
   await ccmc.assertEventTypeNodeUnchecked("et2");
-  await ccmc.assertFrmNodeUnchecked("et2","et2frm1");
-  await ccmc.assertFrmNodeUnchecked("et2","et2frm2");
-  await ccmc.assertEventInstanceNodeUnchecked("et2","et2frm1","et2frm1ei1");
-  await ccmc.assertEventInstanceNodeUnchecked("et2","et2frm1","et2frm1ei2");
-  await ccmc.assertEventInstanceNodeUnchecked("et2","et2frm1","et2frm1ei3");
-  await ccmc.assertEventInstanceNodeUnchecked("et2","et2frm2","et2frm2ei1");
-  await ccmc.assertEventInstanceNodeUnchecked("et2","et2frm2","et2frm2ei2");
+  await ccmc.assertFrmNodeUnchecked("et2", "et2frm1");
+  await ccmc.assertFrmNodeUnchecked("et2", "et2frm2");
+  await ccmc.assertEventInstanceNodeUnchecked("et2", "et2frm1", "et2frm1ei1");
+  await ccmc.assertEventInstanceNodeUnchecked("et2", "et2frm1", "et2frm1ei2");
+  await ccmc.assertEventInstanceNodeUnchecked("et2", "et2frm1", "et2frm1ei3");
+  await ccmc.assertEventInstanceNodeUnchecked("et2", "et2frm2", "et2frm2ei1");
+  await ccmc.assertEventInstanceNodeUnchecked("et2", "et2frm2", "et2frm2ei2");
 });
 
 /**
  * This test mocks some random events for CCMC
- * then selects some specific event_type, 
+ * then selects some specific event_type,
  * then reloads the page and validates event_types should still be checked,
  */
 test("Selected event_types should still be selected after page reload", async ({ page, browser }, info) => {
   // mocked event data
   const events = {
-    CCMC : {
+    CCMC: {
       et0: {
         et0frm1: {
           et0frm1ei1: {},
@@ -134,7 +134,7 @@ test("Selected event_types should still be selected after page reload", async ({
           et3frm1ei1: {},
           et3frm1ei2: {}
         }
-      },
+      }
     }
   };
 
@@ -168,4 +168,3 @@ test("Selected event_types should still be selected after page reload", async ({
   await ccmc.assertEventTypeNodeChecked("et2");
   await ccmc.assertEventTypeNodeUnchecked("et3");
 });
-
