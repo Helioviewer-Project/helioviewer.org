40a0a942b6d628f7f89655b67d55d20f7471f8b2
Daniel Garcia Briseno <dgarciab@gs67-necdet.ndc.nasa.gov>
2025-03-31 11:59:27 -0400

    Fix coordinator_url in Config.js
    We can specify the location of the coordinator server in the config.
    This is already integrated, but it was not being exposed.

--- resources/js/Utility/Config.js
+++ resources/js/Utility/Config.js
@@ -108,7 +108,8 @@ var Config = Class.extend(
             'regenerateMovieThreshold': this.params["regenerate_movie_threshold"],
             'enableHelios'            : this.params["enable_helios_backlinks"],
             'jhelioviewerHost'        : this.params["jhelioviewer_host"],
-            'obstime_alert_dt'        : this.params["obstime_alert_dt"]
+            'obstime_alert_dt'        : this.params["obstime_alert_dt"],
+            'coordinator_url'         : this.params["coordinator_url"],
         };
     }
 });
