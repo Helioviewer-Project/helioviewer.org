a793d6ad4614d6017770da8575fc02d647a9fa7b
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2025-04-02 14:54:46 -0400

    Set API url for 3D render

--- resources/js/3d/main.jsx
+++ resources/js/3d/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Coordinator } from "./coordinates/coordinator";
+import { SetHelioviewerApiUrl } from "@helioviewer/sun";
 import Viewport3D from "./viewport3d";
 
 /** Render the 3D view. */
-window.Init3D = (coordinator_url) =>  createRoot(document.getElementById("view-3d")).render(
-  <Viewport3D coordinator={new Coordinator(coordinator_url)} />
-);
+window.Init3D = (coordinator_url, apiUrl) => {
+  SetHelioviewerApiUrl(apiUrl + "?action=");
+  createRoot(document.getElementById("view-3d")).render(
+    <Viewport3D coordinator={new Coordinator(coordinator_url)} />
+  );
+}
