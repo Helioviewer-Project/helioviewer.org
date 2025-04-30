914080de6c74d34104d60d19cc6391c17bd0c6f6
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2025-04-30 12:14:42 -0400

    Correct observatory names for horizons.

--- resources/js/3d/coordinates/horizons.js
+++ resources/js/3d/coordinates/horizons.js
@@ -21,7 +21,37 @@ class Horizons {
      * @returns {Promise<CoordinateList>}
      */
     GetLocations(observatory, start, end) {
-        return this.coordinator.Position(observatory, start.toISOString(), end.toISOString());
+        return this.coordinator.Position(Horizons.getObservatoryByName(observatory), start.toISOString(), end.toISOString());
+    }
+
+    /**
+     * Maps Helioviewer observatory names to observatory names supported by
+     * JPL Horizons
+     * @param {string} observatory
+     * @returns {string}
+     */
+    static getObservatoryByName(observatory) {
+        const mapping = {
+            "goes-r": "goes-18",
+            // I could not find IRIS in the Horizons database
+            // https://ssd.jpl.nasa.gov/horizons/time_spans.html
+            // 399 maps to earth.
+            "iris": "399",
+            "rhessi": "399",
+            "trace": "399",
+            "yohkoh": "399",
+            "hinode": "399",
+            "mlso": "399",
+            "proba2": "399",
+            "stereo_a": "stereo-a",
+            "stereo_b": "stereo-b"
+        };
+
+        const lowercase = observatory.toLowerCase();
+        if (mapping.hasOwnProperty(lowercase)) {
+            return mapping[lowercase];
+        }
+        return observatory;
     }
 }
 
