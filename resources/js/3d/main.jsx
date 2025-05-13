3529fa333bc9ab36f6631e4fca05969b9a1d24fd
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2025-05-13 14:29:52 -0400

    add enable3d to helioviewer state and load3d when it's enabled

--- resources/js/3d/main.jsx
+++ resources/js/3d/main.jsx
@@ -4,20 +4,12 @@ import { Coordinator } from "./coordinates/coordinator";
 import { SetHelioviewerApiUrl } from "@helioviewer/sun";
 import Viewport3D from "./viewport3d";
 
-// Tracks if 3D mode is active.
-// This is toggled when the 3D button is clicked
-let is3dEnabled = false;
-
 // Tracks if 3D mode has been turned on at least once.
 // This is a performance optimization to make sure we don't load all 3D
 // assets until the user requests it.
 // Once 3D has been launched and assets have been loaded, then it becomes
 // a simple show/hide.
 let is3dLaunched = false;
-
-// This function can be used by the main HV application to check if
-// 3D mode is currently visible.
-window.is3dViewActive = () => is3dEnabled;
 
 /** Handles what to do if 3D rendering fails */
 const onFail = (error) => {
@@ -30,9 +22,16 @@ const onFail = (error) => {
  * and parses it into the required parameters for the 3D viewport module.
  */
 function getHvState() {
-  const state = JSON.parse(localStorage.getItem("settings"));
-  const date = state.state.date;
-  const layers = Object.values(state.state.tileLayers).map((hvLayer) => {
+  const date = Helioviewer.userSettings.get("state.date");
+  const enabled = Helioviewer.userSettings.get("state.enable3d");
+  /**
+   * @typedef {Object} LayerObject
+   * @property {string} sourceId - The ID of the source
+   * @property {number} opacity - The opacity of the layer
+   * @property {boolean} visible - The visibility of the layer
+   * @property {string} observatory - The name of the observatory
+   */
+  const layers = Object.values(Helioviewer.userSettings.get("state.tileLayers")).map((hvLayer) => {
     return {
       sourceId: hvLayer.sourceId,
       observatory: hvLayer.uiLabels[0].name,
@@ -40,26 +39,16 @@ function getHvState() {
       opacity: hvLayer.opacity
     };
   });
-  return [layers, date];
+  return [layers, date, enabled];
 }
 
-/**
-/**
- * @typedef {Object} LayerObject
- * @property {string} sourceId - The ID of the source
- * @property {number} opacity - The opacity of the layer
- * @property {boolean} visible - The visibility of the layer
- * @property {string} observatory - The name of the observatory
- */
-function render(root, visible, coordinator_url) {
-  // Extract parameters for the 3D viewport from the HV State
-  const [layers, observationTime] = getHvState();
+function render(root, launched, enabled, layers, date, coordinator_url) {
   root.render(
     <Viewport3D
-      active={is3dLaunched}
+      active={launched}
       layers={layers}
-      date={observationTime}
-      visible={visible}
+      date={date}
+      visible={enabled}
       coordinator={new Coordinator(coordinator_url)}
       onFail={onFail}
       onLoadStart={() => helioviewerWebClient.startLoading()}
@@ -68,49 +57,19 @@ function render(root, visible, coordinator_url) {
   );
 }
 
-/** Handle to HTML element which shows the 3D view */
-const viewport3dRoot = document.getElementById("view-3d");
-
-function init3DButtons(rerender_func, api_url) {
-  /** Button to toggle between 3D and 2D views */
-  const toggleButtons = document.querySelectorAll(".js-3d-toggle");
-
-  /**
-   * Set up the toggle button that switches between the 2D and 3D viewports
-   * When clicked, change the z-index of the 3D view so it appears in front.
-   * @note We need to use z-indexes instead of the css display property because
-   *       Helioviewer depends on the position of the 2D viewport. Changing the
-   *       display property will effect the viewport's position.
-   */
-  toggleButtons.forEach((btn) => {
-    btn.onclick = () => {
-      // Turn on once.
-      // This enables loading all 3D data whenever the observation date changes.
-      if (!is3dLaunched) {
-        is3dLaunched = true;
-        // count activations
-        fetch(api_url + "?action=enable3D");
-      }
-      is3dEnabled = !is3dEnabled;
-      btn.classList.toggle("active");
-      document.querySelectorAll(".toggle3d").forEach((element) => {
-        element.classList.toggle("masked");
-      });
-      viewport3dRoot.style.zIndex = viewport3dRoot.style.zIndex == "-10" ? "0" : "-10";
-      rerender_func(is3dEnabled);
-    };
-  });
-}
-
-/** Render the 3D view. */
+/**
+ * Render the 3D view.
+ */
 window.Init3D = (coordinator_url, apiUrl) => {
   SetHelioviewerApiUrl(apiUrl + "?action=");
   // Create root and begin executing the component via the render call
   const root = createRoot(document.getElementById("view-3d"));
-  render(root, false, coordinator_url);
 
-  // Initialize the event listeners on the buttons that enable/disable 3D mode.
-  init3DButtons((visible) => render(root, visible, coordinator_url), apiUrl);
+  const toggleButton = document.querySelector('.js-3d-toggle');
+  toggleButton.addEventListener('click', () => {
+    Helioviewer.userSettings.set("state.enable3d", !Helioviewer.userSettings.get("state.enable3d"));
+    $(document).trigger("update-external-datasource-integration");
+  });
 
   /**
    * Listen for changes to state.
@@ -118,6 +77,52 @@ window.Init3D = (coordinator_url, apiUrl) => {
    * re-render whenever state is updated
    */
   $(document).on("update-external-datasource-integration", () => {
-    render(root, is3dEnabled, coordinator_url);
+    const [layers, date, enabled] = getHvState();
+    update3DButtonCss(enabled);
+    hideDisabledElements(enabled);
+    update3dViewport(enabled);
+    if (!is3dLaunched && enabled) {
+      is3dLaunched = true;
+      fetch(apiUrl + "?action=enable3D");
+    }
+    render(root, is3dLaunched, enabled, layers, date, coordinator_url);
   });
 };
+
+/**
+ * Update 3D CSS on 3D activation buttons for 3D mode
+ * @param {boolean} enable3d 3D enabled/disabled
+ */
+function update3DButtonCss(enable3d) {
+  document.querySelectorAll('.js-3d-toggle').forEach(btn => {
+    if (enable3d) {
+      btn.classList.add('active');
+    } else {
+      btn.classList.remove('active');
+    }
+  });
+}
+
+/**
+ * Hides HTML elements which should be hidden when 3D mode is enabled
+ * @param {boolean} enable3d  3D enabled/disabled
+ */
+function hideDisabledElements(enable3d) {
+  document.querySelectorAll('.toggle3d').forEach(element => {
+    if (enable3d) {
+      element.classList.add('masked');
+    } else {
+      element.classList.remove('masked');
+    }
+  });
+}
+
+/**
+ * Shows/Hides the 3D viewport for 3D mode
+ * @param {boolean} enable3d 3D enabled/disabled
+ */
+function update3dViewport(enable3d) {
+  /** Handle to HTML element which shows the 3D view */
+  const viewport3dRoot = document.getElementById("view-3d");
+  viewport3dRoot.style.zIndex = enable3d ? "0" : "-10";
+}
